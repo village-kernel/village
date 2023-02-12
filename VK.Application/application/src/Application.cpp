51af7d13411a87e9d1ec80324711e9e45acabfdd
aicodeone <277579066@qq.com>
2023-02-12 22:02:40 +0800

    VK.Application：测试Gpio的Hal库代码

--- VK.Application/application/src/Application.cpp
+++ VK.Application/application/src/Application.cpp
@@ -8,15 +8,6 @@
 #include "Application.h"
 #include "HalHeaders.h"
 
-#ifdef __cplusplus
-extern "C"
-#endif
-void SysTick_Handler(void)
-{
-	HAL_IncTick();
-	HAL_SYSTICK_IRQHandler();
-}
-
 
 /// Constructor
 Application::Application()
@@ -27,27 +18,17 @@ Application::Application()
 /// Initialize
 void Application::Initialize()
 {
-	HAL_Init();
-
-	__GPIOA_CLK_ENABLE();
-	GPIO_InitTypeDef GPIO_InitStructure;
-
-	GPIO_InitStructure.Pin = GPIO_PIN_3;
-
-	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStructure.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+    led.Initialize(Gpio::_ChA, 3, Gpio::_Low);
 }
 
 
 /// Execute
 void Application::Execute()
 {
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
-    HAL_Delay(100);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-    HAL_Delay(100);
+    led.Set();
+    System::DelayMs(100);
+    led.Clear();
+    System::DelayMs(100);
 }
 
 
