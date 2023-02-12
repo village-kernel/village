9491aa3cdd82d0da29fa1a916c59f8fe63362f85
aicodeone <277579066@qq.com>
2023-02-12 22:01:22 +0800

    VK.Hardware：增加Gpio，Gpi，Gpo相关代码

---
+++ VK.Hardware/HAL/ST/stm32f4xx/src/Gpo.cpp
@@ -0,0 +1,52 @@
+//###########################################################################
+// Gpo.cpp
+// Hardware layer that manages a single GPIO output pin
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#include "Gpo.h"
+
+
+///Constructor
+Gpo::Gpo()
+{
+}
+
+
+///Initializes the GPIO pin, does not actually perform any configuration
+void Gpo::Initialize(GpioChannel channel, uint16_t pin, GpioState initState = _Low)
+{
+	//Call the parent initialize first
+	Gpio::Initialize(channel, pin);
+
+	setReg = &(baseReg->BSRR);
+	clearReg = &(baseReg->BSRR);
+
+	//Set the initial value
+	//Set the correct mode
+	Write(initState);
+	ConfigMode(_Ouput);
+}
+
+
+///Configures the output type (push-pull or open drain)
+void Gpo::ConfigType(GpioOutType outType)
+{
+	baseReg->OTYPER = (baseReg->OTYPER & ~bitMask) | (outType << pinNum);
+}
+
+
+///Writes a specified state into the pin
+void Gpo::Write(GpioState state)
+{
+	if (_Low == state) Clear();
+	else Set();
+}
+
+
+///Toggles the pin value
+void Gpo::Toggle()
+{
+	if (_Low == GetOutputState()) Set();
+	else Clear();
+}
