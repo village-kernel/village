b82d95cae0b038adac447de57ceade889e7996cf
aicodeone <277579066@qq.com>
2023-02-17 21:57:49 +0800

    vk.application：重新添加回来，解决冲突

---
+++ vk.application/Start.cpp
@@ -0,0 +1,23 @@
+//###########################################################################
+// Start.cpp
+//
+// Top level singleton class that manages entire application
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#include "System.h"
+#include "Kernel.h"
+
+
+///Main entry function
+int main(void)
+{
+	System::Initialize();
+	System::ConfigureMPU();
+	System::ConfigurePower();
+	System::ConfigureForXtal();
+
+	Kernel::Initialize();
+	Kernel::UpdateParams();
+	Kernel::Execute();
+}
