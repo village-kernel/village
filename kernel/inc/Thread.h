827862556b4fd17eb9f10a359c4d4762cf3c81e5
aicodeone <277579066@qq.com>
2023-02-19 20:15:46 +0800

    Thread：初步使用PendSV完成任务调度，初步实现多任务功能。下一步：完善任务调度。
    小知识：
    __attribute__ ((naked)) 是所谓的“裸函数”，对于这种函数，编译器不会生成任何函数入口代码和退出代码。
    这种函数一般应用在与操作系统内核相关的代码中，如中断处理函数、钩子函数等。

--- kernel/inc/Thread.h
+++ kernel/inc/Thread.h
@@ -11,11 +11,69 @@
 
 extern "C" 
 {
-
+void IdleTask(void);
 void Task0(void);
 void Task1(void);
 
 void StartScheduler(void);
 }
+
+
+///Thread
+class Thread
+{
+private:
+	enum TaskState 
+	{
+		Running = 0,
+		Blocked = 1,
+	};
+
+    enum Access
+    {
+        Unprivileged = 0,
+        Privileged = 1,
+    };
+
+	struct Task 
+	{
+		void (*handler)(void);
+		TaskState state;
+		uint32_t psp;
+		uint32_t waitToTick;
+
+		Task()
+			:handler(0),
+			state(TaskState::Running),
+			psp(0),
+			waitToTick(0)
+		{}
+	};
+
+    //Static constants
+    static const unsigned int end_stack = 0x2001c000;
+	static const unsigned int task_number_size = 16;
+	static const unsigned int task_stack_szie = 1024;
+
+    //Members
+    static unsigned int storedIndex;
+   	static unsigned int taskIndex;
+	static unsigned int sysTicks;
+	static Task tasks[task_number_size];
+
+    //Methods
+    static void Rescheduler(Access access);
+public:
+    ///Methods
+    Thread();
+    static void CreateTask(void (*handler)());
+    static void SaveTaskPSP(uint32_t psp);
+    static uint32_t GetTaskPSP();
+    static uint32_t GetTaskHandler();
+    static void SelectNextTask();
+    static void Sleep(uint32_t ticks);
+    static void SystickCount();
+    static void TaskOperator(uint32_t* SP);
+};
 
 #endif // !__THREAD_H__
--- kernel/src/Thread.cpp
+++ kernel/src/Thread.cpp
@@ -6,190 +6,248 @@
 //###########################################################################
 #include "Thread.h"
 
-#include "Gpo.h"
+unsigned int Thread::storedIndex = 0;
+unsigned int Thread::taskIndex = 0;
+unsigned int Thread::sysTicks = 0;
+Thread::Task Thread::tasks[Thread::task_number_size];
 
-struct TaskStack {
-	unsigned int r4;
-	unsigned int r5;
-	unsigned int r6;
-	unsigned int r7;
-	unsigned int r8;
-	unsigned int r9;
-	unsigned int r10;
-	unsigned int r11;
-	unsigned int r0;
-	unsigned int r1;
-	unsigned int r2;
-	unsigned int r3;
-	unsigned int r12;
-	unsigned int lr;
-	unsigned int pc;
-	unsigned int xpsr;
-
-	TaskStack():
-		r4(0),
-		r5(0),
-		r6(0),
-		r7(0),
-		r8(0),
-		r9(0),
-		r10(0),
-		r11(0),
-		r0(0),
-		r1(0),
-		r2(0),
-		r3(0),
-		r12(0),
-		lr(0),
-		pc(0),
-		xpsr(0)
-	{}
-};
-
-
-void Task0(void)
+///Constructor
+Thread::Thread()
 {
-	// Gpo led;
-	
-	// led.Initialize(Gpio::_ChE, 5, Gpio::_Low);
-
-	// while(1) 
-	// {
-	// 	led.Set();
-	// 	System::DelayMs(500);
-	// 	led.Clear();
-	// 	System::DelayMs(500);
-	// }
-
-	while(1) {
-		printf("1111\n");
-	}
-}
-
-void Task1(void)
-{
-	// Gpo led;
-	
-	// led.Initialize(Gpio::_ChA, 8, Gpio::_Low);
-
-	// while (1)
-	// {
-	// 	led.Set();
-	// 	System::DelayMs(500);
-	// 	led.Clear();
-	// 	System::DelayMs(500);
-	// }
-
-	while(1) {
-		printf("2222\n");
-	}
 }
 
 
-class Task
+///Create new task
+void Thread::CreateTask(void (*handler)())
 {
-private:
-	static const unsigned int task_stack = 0x20000000;
-	static const unsigned int task_number_size = 16;
-	static const unsigned int task_stack_szie = 1024;
-	static const unsigned int main_stack = task_stack + (128 * 1024);
-public:
-	unsigned int storedIndex;
-   	unsigned int taskIndex;
-	unsigned int tasksPSP[task_number_size] = { 0 };
-	
-	///Constructor
-	Task()
-		:storedIndex(0),
-		taskIndex(0)
-    {
-    }
+	//Check
+	if(storedIndex >= task_number_size) return;
 
-	///Create new task
-	void CreateTask(void (*task)())
+	//Init idle if it is not found
+	if (handler != &IdleTask)
 	{
-		if(storedIndex >= task_number_size) return;
-
-		uint32_t *newTaskPSP = (uint32_t*)(main_stack - (storedIndex + 1) * task_stack_szie);
-
-		// fill dummy stack frame
-		*(--newTaskPSP) = 0x01000000u; // Dummy xPSR, just enable Thumb State bit;
-		*(--newTaskPSP) = (uint32_t)task; // PC
-		*(--newTaskPSP) = 0xFFFFFFFDu; // LR with EXC_RETURN to return to Thread using PSP
-		*(--newTaskPSP) = 0x0; // Dummy R12
-		*(--newTaskPSP) = 0x0; // Dummy R3
-		*(--newTaskPSP) = 0x0; // Dummy R2
-		*(--newTaskPSP) = 0x0; // Dummy R1
-		*(--newTaskPSP) = 0x0; // Dummy R0
-		*(--newTaskPSP) = 0x0; // Dummy R11
-		*(--newTaskPSP) = 0x0; // Dummy R10
-		*(--newTaskPSP) = 0x0; // Dummy R9
-		*(--newTaskPSP) = 0x0; // Dummy R8
-		*(--newTaskPSP) = 0x0; // Dummy R7
-		*(--newTaskPSP) = 0x0; // Dummy R6
-		*(--newTaskPSP) = 0x0; // Dummy R5
-		*(--newTaskPSP) = 0x0; // Dummy R4
-
-		tasksPSP[storedIndex++] = (uint32_t)newTaskPSP;
+		if (tasks[0].handler != IdleTask)
+		{
+			CreateTask(IdleTask);
+		}
 	}
 
-	///Save task PSP
-	void SaveTaskPSP(uint32_t psp)
+	//Calculate new task psp address
+	uint32_t *psp = (uint32_t*)(end_stack - (storedIndex + 1) * task_stack_szie);
+
+	//Fill dummy stack frame
+	*(--psp) = 0x01000000u; // Dummy xPSR, just enable Thumb State bit;
+	*(--psp) = (uint32_t)handler; // PC
+	*(--psp) = 0xFFFFFFFDu; // LR with EXC_RETURN to return to Thread using PSP
+	*(--psp) = 0x0; // Dummy R12
+	*(--psp) = 0x0; // Dummy R3
+	*(--psp) = 0x0; // Dummy R2
+	*(--psp) = 0x0; // Dummy R1
+	*(--psp) = 0x0; // Dummy R0
+	*(--psp) = 0x0; // Dummy R11
+	*(--psp) = 0x0; // Dummy R10
+	*(--psp) = 0x0; // Dummy R9
+	*(--psp) = 0x0; // Dummy R8
+	*(--psp) = 0x0; // Dummy R7
+	*(--psp) = 0x0; // Dummy R6
+	*(--psp) = 0x0; // Dummy R5
+	*(--psp) = 0x0; // Dummy R4
+
+	//Stored task info
+  tasks[storedIndex].psp = (uint32_t)psp;
+	tasks[storedIndex].handler = handler;
+	tasks[storedIndex].state = TaskState::Running;
+	storedIndex++;
+}
+
+
+///Save task PSP
+void Thread::SaveTaskPSP(uint32_t psp)
+{
+	tasks[taskIndex].psp = psp;
+}
+
+
+///Get current task psp
+uint32_t Thread::GetTaskPSP()
+{
+	return tasks[taskIndex].psp;
+}
+
+
+///Get currenttask handler
+uint32_t Thread::GetTaskHandler()
+{
+	return (uint32_t)tasks[taskIndex].handler;
+}
+
+
+///Select next task
+void Thread::SelectNextTask()
+{
+	//Check all task state
+	for(uint32_t i = 0; i < storedIndex; i++) 
 	{
-		tasksPSP[taskIndex] = psp;
+		if(tasks[i].state == TaskState::Blocked) 
+		{
+			if(sysTicks >= tasks[i].waitToTick)
+			{
+				tasks[i].state = TaskState::Running;
+			}
+		}
 	}
 
-	///Get current task psp
-	uint32_t GetTaskPSP()
+	//Round-Robin scheduler
+	while (1)
 	{
-		return tasksPSP[taskIndex];
-	}
-
-    /// Select next task
-    void SelectNextTask()
-    {
 		if (++taskIndex >= storedIndex) taskIndex = 0;
-    }
-};
 
-Task *task = new Task();
+		if(tasks[taskIndex].state == TaskState::Running) break;
+	}
+}
+
+
+///Rescheduler task
+void Thread::Rescheduler(Thread::Access access)
+{
+	if (Access::Privileged == access)
+	{
+		// trigger PendSV directly
+		SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
+	}
+	else
+	{
+		// call Supervisor exception to get Privileged access
+		__ASM("SVC #255");
+	}
+}
+
+
+///Thread sleep
+void Thread::Sleep(uint32_t ticks)
+{
+	if(taskIndex)
+	{
+		tasks[taskIndex].state = TaskState::Blocked;
+		tasks[taskIndex].waitToTick = sysTicks + ticks;
+		Rescheduler(Access::Unprivileged);
+	}
+}
+
+
+///Systick count
+void Thread::SystickCount()
+{
+	sysTicks++;
+	Rescheduler(Access::Privileged);
+}
+
+
+///Execute task requests
+void Thread::TaskOperator(uint32_t* sp)
+{
+	//Get the address of the instruction saved in PC
+	uint8_t *pInstruction = (uint8_t*)(sp[6]);
+
+	//Go back 2 bytes (16-bit opcode)
+	pInstruction -= 2;
+
+	//Get the opcode, in little endian
+	uint8_t svcNumber = *pInstruction;
+
+	switch(svcNumber)
+	{
+		case 0xFF:
+			//Trigger PendSV
+			SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
+			break;
+		default:
+			break;
+	}
+}
 
 extern "C" 
 {
+
+#include "Gpo.h"
+
+	void IdleTask(void)
+	{
+		while(1)
+		{
+			__ASM("NOP");
+		}
+	}
+
+	void Task0(void)
+	{
+		Gpo led;
+
+		led.Initialize(Gpio::_ChE, 5, Gpio::_Low);
+
+		while(1) 
+		{
+			led.Set();
+			Thread::Sleep(500);
+			led.Clear();
+			Thread::Sleep(500);
+		}
+	}
+
+	void Task1(void)
+	{
+		Gpo led;
+
+		led.Initialize(Gpio::_ChA, 8, Gpio::_Low);
+
+		while (1)
+		{
+			led.Set();
+			Thread::Sleep(500);
+			led.Clear();
+			Thread::Sleep(500);
+		}
+	}
+
 	void saveTaskPSP(uint32_t psp)
 	{
-		task->SaveTaskPSP(psp);
+		Thread::SaveTaskPSP(psp);
 	}
 
 	uint32_t getTaskPSP()
 	{
-		return task->GetTaskPSP();
+		return Thread::GetTaskPSP();
 	}
 
 	void SysTick_Handler(void)
 	{
+		Thread::SystickCount();
+	}
+
+	__attribute__ ((naked)) void PendSV_Handler(void)
+	{
 		//Save LR back to main, must do this firstly
 		__ASM("push {lr}");
 
-		//Gets the current msp
+		//Gets the current psp
 		__ASM("mrs r0, psp");
 
-		//Save R4 to R11 to msp frame stack
+		//Save R4 to R11 to psp frame stack
 		__ASM("stmdb r0!, {r4-r11}");
 
-		//Save current value of msp
+		//Save current value of psp
 		__ASM("bl saveTaskPSP");
 
 		//Select next task
-		task->SelectNextTask();
+		Thread::SelectNextTask();
 
-		//Get its past PSP value, return PSP is in R0
+		//Get its past psp value, return psp is in R0
 		__ASM("bl getTaskPSP"); 
 
-		//Retrieve R4-R11 from msp frame stack
+		//Retrieve R4-R11 from psp frame stack
 		__ASM("ldmia r0!, {r4-r11}");
 		
-		//Update msp
+		//Update psp
 		__ASM("msr psp, r0");
 
 		//Exit
@@ -197,32 +255,52 @@ extern "C"
 		__ASM("bx lr");
 	}
 
-	void StartScheduler()
+	__attribute__ ((naked)) void SVC_Handler(void)
 	{
-		//Create tasks
-		task->CreateTask(Task0);
-		task->CreateTask(Task1);
+		__ASM("TST LR, 4"); // check LR to know which stack is used
+		__ASM("ITE EQ"); // 2 next instructions are conditional
+		__ASM("MRSEQ R0, MSP"); // save MSP if bit 2 is 0
+		__ASM("MRSNE R0, PSP"); // save PSP if bit 2 is 1
+		__ASM("B SVC_Handler_Main"); // pass R0 as the argument
+	}
 
-		//Prepare PSP of the first task, return PSP in R0 and set PSP
-		__ASM("bl getTaskPSP");
-		__ASM("msr psp, r0"); 
+	void SVC_Handler_Main(uint32_t* sp)
+	{
+		Thread::TaskOperator(sp);
+	}
 
-		//Change to use PSP, set bit[1] SPSEL
-		__ASM("mrs r0, control");
-		__ASM("orr r0, r0, #2");
-		__ASM("msr control, r0");
-
+	void ConfigSysTick()
+	{
 		//Enable systick interrupt
 		SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
 		
 		//Set systick reload value
-		SysTick->LOAD = 210000;
+		SysTick->LOAD = 21000;
 		
 		//Clear systick current value
 		SysTick->VAL = 0;
 		
 		//Enable systick
 		SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+	}
+
+	void StartScheduler()
+	{
+		//Create tasks
+		Thread::CreateTask(Task0);
+		Thread::CreateTask(Task1);
+
+		//Prepare PSP of the first task, return PSP in R0 and set PSP
+		__ASM("bl getTaskPSP");
+		__ASM("msr psp, r0");
+
+		//Change to use PSP, set bit[1] SPSEL
+		__ASM("mrs r0, control");
+		__ASM("orr r0, r0, #2");
+		__ASM("msr control, r0");
+
+		//Configure system tick
+		ConfigSysTick();
 
 		//Move to Unprivileged level, Set bit[0] nPRIV
 		__ASM("mrs r0, control");
@@ -230,7 +308,7 @@ extern "C"
 		__ASM("msr control, r0");
 	
 		//Get the handler of the first task by tracing back from PSP which is at R4 slot
-		void (*Handler)() = (void (*)())((uint32_t*)(((TaskStack*)task->GetTaskPSP())->pc));
+		void (*Handler)() = (void (*)())((uint32_t*)(Thread::GetTaskHandler()));
 
 		//Execute the handler
 		Handler();
