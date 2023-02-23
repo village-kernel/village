90b31a09bc536104910b3e934234b0571eb0511d
aicodeone <277579066@qq.com>
2023-02-23 23:32:26 +0800

    Application3：测试flash和sdcard文件系统功能

--- vk.application/application/src/Application3.cpp
+++ vk.application/application/src/Application3.cpp
@@ -19,33 +19,39 @@ Application3::Application3()
 ///Initialize
 void Application3::Initialize()
 {
-	uint8_t wString[] = "hello vk.kernel";
-	uint8_t rString[] = "";
-
-	//HWManager::Instance()->spiFlash.Write(wString, 16, 0);
-	HWManager::Instance()->spiFlash.Read(rString, 16, 0);
-
 	gui.Initialize(&(HWManager::Instance()->ili9488));
-	gui.disp.ShowString(0, 0, rString);
+	
+	gui.disp.ShowString(0, 0, (uint8_t*)"hello vk.kernel");
 
 	FATFS fs; DIR file_dir; FILINFO fileinfo;
 
-	if (f_mount(&fs, "1:", 1) == FR_OK)
+	const TCHAR* path[] = { "0:", "1:" };
+
+	uint16_t disp_y = 32;
+	
+	for (uint8_t i = 0; i < 2; i++)
 	{
-		if (f_opendir(&file_dir, "1:") == FR_OK)
+		gui.disp.ShowString(0, disp_y, (uint8_t*)"storage");
+		gui.disp.ShowString(60, disp_y, (uint8_t*)path[i]);
+		disp_y += 16;
+
+		if (f_mount(&fs, path[i], 1) == FR_OK)
 		{
-			uint16_t disp_y = 16;
-			while(1)
+			if (f_opendir(&file_dir, path[i]) == FR_OK)
 			{
-				FRESULT res = f_readdir(&file_dir, &fileinfo);
-				if (res != FR_OK || fileinfo.fname[0] == 0) break;
-				
-				gui.disp.ShowString(0, disp_y, (uint8_t*)fileinfo.fname);
-				disp_y += 16;
+				while(1)
+				{
+					FRESULT res = f_readdir(&file_dir, &fileinfo);
+					if (res != FR_OK || fileinfo.fname[0] == 0) break;
+					
+					gui.disp.ShowString(0, disp_y, (uint8_t*)fileinfo.fname);
+					disp_y += 16;
+				}
 			}
+			f_closedir(&file_dir);
+			f_unmount(path[i]);
+			disp_y += 16;
 		}
-		f_closedir(&file_dir);
-		f_unmount("1:");
 	}
 }
 
