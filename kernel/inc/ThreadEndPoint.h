8dc62b0e7b69e50708126f48239489ba163f138f
aicodeone <277579066@qq.com>
2023-03-06 12:51:34 +0800

    ThreadEndPoint：增加ThreadEndPoint模板，给C++类创建Task用的

---
+++ kernel/inc/ThreadEndPoint.h
@@ -0,0 +1,45 @@
+//###########################################################################
+// ThreadEndPoint.h
+// Declarations of the functions that manage thread end point
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __THREAD_END_POINT_H__
+#define __THREAD_END_POINT_H__
+
+#include "Thread.h"
+
+//ThreadEndPoint class template 
+template<typename User, typename Handler>
+class ThreadEndPoint
+{
+private:
+	//Declarations static members
+	static User* user;
+	static Handler handler;
+	
+	//Thread Handler
+	static void TaskHandler()
+	{
+		//Execute task
+		if (user != 0) { (user->*handler)(); }
+		//Exit task
+		Thread::Exit();
+	}
+public:
+	//Set task end point and create task
+	static void CreateTask(User* _user, Handler _handler)
+	{
+		user = _user;
+		handler = _handler;
+		Thread::CreateTask(ThreadEndPoint::TaskHandler);
+	}
+};
+
+//Definitions static members
+template<typename User, typename Handler>
+User* ThreadEndPoint<User, Handler>::user;
+template<typename User, typename Handler>
+Handler ThreadEndPoint<User, Handler>::handler;
+
+#endif //!__THREAD_END_POINT_H__
