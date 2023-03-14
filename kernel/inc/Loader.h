6ada376f5bb30700d28f06d0f4f57542b13bf02b
aicodeone <277579066@qq.com>
2023-03-14 19:36:55 +0800

    Loader：初步解析elf文件

--- kernel/inc/Loader.h
+++ kernel/inc/Loader.h
@@ -8,7 +8,6 @@
 #define __LOADER_H__
 
 #include "Module.h"
-#include "FileStream.h"
 
 
 ///Loader
@@ -22,7 +21,140 @@ private:
 		_ERR    = 1,
 	};
 
-	//Structures
+	enum ELFClass
+	{
+		_ELF_Class_None   = 0,
+		_ELF_Class_32     = 1,
+		_ELF_Class_64     = 2,
+	};
+
+	enum ELFType
+	{
+		_ELF_Type_None    = 0,
+		_ELF_Type_Rel     = 1,
+		_ELF_Type_Exec    = 2,
+		_ELF_Type_Dyn     = 3,
+		_ELF_Type_Core    = 4,
+		_ELF_Type_LO_Proc = 5,
+		_ELF_Type_HI_Proc = 6,
+	};
+
+	enum ELFMachine
+	{
+		_ELF_Machine_None  = 0,
+		_ELF_Machine_ARM   = 40,
+	};
+
+	enum ELFVersion
+	{
+		_ELF_Ver_None    = 0,
+		_ELF_Ver_Current = 1,
+	};
+
+	enum RelocationCode
+	{
+		_R_ARM_NONE       = 0,
+		_R_ARM_ABS32      = 2,
+		_R_ARM_THM_CALL   = 10,
+		_R_ARM_THM_JUMP24 = 30,
+		_R_ARM_TARGET1    = 38,
+		_R_ARM_THM_JUMP11 = 102,
+	};
+
+	enum SectionHeadrType
+	{
+		_SHT_NULL          = 0x00,
+		_SHT_PROGBITS      = 0x01,
+		_SHT_SYMTAB        = 0x02,
+		_SHT_STRTAB        = 0x03,
+		_SHT_RELA          = 0x04,
+		_SHT_HASH          = 0x05,
+		_SHT_DYNAMIC       = 0x06,
+		_SHT_NOTE          = 0x07,
+		_SHT_NOBITS        = 0x08,
+		_SHT_REL           = 0x09,
+		_SHT_SHLIB         = 0x0a,
+		_SHT_DYNSYM        = 0x0b,
+		_SHT_INIT_ARRAY    = 0x0e,
+		_SHT_FINI_ARRAY    = 0x0f,
+		_SHT_PREINIT_ARRAY = 0x10,
+		_SHT_GROUP         = 0x11,
+		_SHT_SYMTAB_SHNDX  = 0x12,
+		_SHT_NUM           = 0x13,
+		_SHT_LOOS          = 0x60000000,
+	};
+
+	enum SymbolType
+	{
+		_STT_NOTYPE     = 0,
+		_STT_OBJECT     = 1,
+		_STT_FUNC       = 2,
+		_STT_SECTION    = 3,
+		_STT_FILE       = 4,
+		_STT_LOPROC     = 13,
+		_STT_HIPROC     = 15,
+	};
+
+	enum SymbolBind
+	{
+		_STB_LOCAL    = 0,
+		_STB_GLOBAL   = 1,
+		_STB_WEAK     = 2,
+		_STB_LOPROC   = 13,
+		_STB_HIPROC   = 15,
+	};
+
+	//Symbol Structures
+	struct SymbolEntry
+	{
+		uint32_t     name;
+		uint32_t     value;
+		uint32_t     size;
+		uint8_t      type : 4;
+		uint8_t      bind : 4;
+		uint8_t      other;
+		uint16_t     index;
+	};
+
+	struct Symbol
+	{
+		uint8_t*     name;
+		SymbolEntry* entry;
+		Symbol*      next;
+	};
+
+	struct SymbolTable
+	{
+		Symbol*      symbols;
+		uint8_t*     sectionName;
+		uint32_t     sectionIndex;
+		SymbolTable* next;
+	};
+
+	struct RelocationEntry
+	{
+		uint32_t offset;
+		uint32_t type:8;
+		uint32_t symbol:8;
+		uint32_t reversed:16;
+	};
+
+	struct Relocation
+	{
+		uint8_t*         name;
+		RelocationEntry* entry;
+		Relocation*      next;
+	};
+
+	struct RelocationTable
+	{
+		Relocation*      relocations;
+		uint8_t*         sectionName;
+		uint32_t         sectionIndex;
+		RelocationTable* next;
+	};
+
+	//Elf header Structures
 	struct ELFHeader
 	{
 		uint8_t  ident[16];
@@ -41,36 +173,7 @@ private:
 		uint16_t sectionHeaderStringTableIndex;
 	};
 
-	struct SectionHeader
-	{
-		uint32_t nameOffset;
-		uint32_t type;
-		uint32_t flags;
-		uint32_t addr;
-		uint32_t offset;
-		uint32_t size;
-		uint32_t link;
-		uint32_t info;
-		uint32_t addrAlign;
-		uint32_t entireSize;
-	};
-
-	struct SymbolTableEntry
-	{
-		uint32_t name;
-		uint32_t value;
-		uint32_t size;
-		uint8_t  info;
-		uint8_t  other;
-		uint16_t index;
-	};
-
-	struct RelocationEntry
-	{
-		uint32_t offset;
-		uint32_t info;
-	};
-
+	//Porgram structures
 	struct ProgramHeader
 	{
 		uint32_t type;
@@ -83,6 +186,7 @@ private:
 		uint32_t align;
 	};
 
+	//Dynamic structures
 	struct DynamicHeader
 	{
 		int32_t tag;
@@ -93,27 +197,53 @@ private:
 		};
 	};
 
-
-	struct ELF32
+	//Section structures
+	struct SectionHeader
 	{
-		ELFHeader       elfHeader;
-		SectionHeader*  sectionHeaders;
+		uint32_t name;
+		uint32_t type;
+		uint32_t flags;
+		uint32_t addr;
+		uint32_t offset;
+		uint32_t size;
+		uint32_t link;
+		uint32_t info;
+		uint32_t addrAlign;
+		uint32_t entireSize;
+	};
+
+	struct Section
+	{
+		SectionHeader* header;
+		uint8_t*       name;
+		union
+		{
+			uint8_t*         data;
+			SymbolEntry*     symEntries;
+			RelocationEntry* relEntries;
+		};
+		Section*       next;
+	};
+
+	//ELF structure
+	struct ELF
+	{
+		uint8_t*         map;
+		ELFHeader*       header;
+		Section*         sections;
+		SymbolTable*     symTabs;
+		RelocationTable* relTabs;
 	};
 	
-	//Static constants
-	static const int32_t elf_header_size = sizeof(ELFHeader);
-	static const int32_t sec_header_size = sizeof(SectionHeader);
-
 	//Members
-	FileStream file;
-	ELF32      elf32;
+	ELF elf;
 
 	//Methods
-	void LoadElf32(const char* path);
-	int ReadELFHeader();
-	int ReadSectionHeaders();
-	int ReadSymbolTableEntries();
-	int ReadRelocationEntries();
+	int LoadElf(const char* path);
+	int ParserElfHeader();
+	int ParserElfSection();
+	int ParserSymbolEntries();
+	int ParserRelocationEntries();
 public:
 	//Methods
 	void Initialize();
--- kernel/src/Loader.cpp
+++ kernel/src/Loader.cpp
@@ -6,58 +6,251 @@
 //###########################################################################
 #include "Kernel.h"
 #include "Loader.h"
+#include "FileStream.h"
+#include "string.h"
 
 
 ///Loader Initialize
 void Loader::Initialize()
 {
-	LoadElf32("1:app-striped.elf");
+	if (LoadElf("1:app-striped.elf") != _OK) return;
 
-	ReadELFHeader();
+	if (ParserElfHeader() != _OK) return;
 
-	ReadSectionHeaders();
+	if (ParserElfSection() != _OK) return;
 
-	ReadSymbolTableEntries();
+	if (ParserSymbolEntries() != _OK) return;
 
-	ReadRelocationEntries();
+	if (ParserRelocationEntries() != _OK) return;
 }
 
 
-void Loader::LoadElf32(const char* path)
+///Loader load elf from path
+int Loader::LoadElf(const char* path)
 {
-	file.Open(path);
+	FileStream file;
+
+	if (FR_OK == file.Open(path, FileStream::_Read))
+	{
+		int size = file.Size();
+		elf.map = (uint8_t*)malloc(size);
+
+		if (file.Read(elf.map, size) == size)
+		{
+			file.Close(); return _OK;
+		}
+
+		file.Close();
+	}
+
+	return _ERR;
 }
 
 
-int Loader::ReadELFHeader()
+///Parser elf header
+int Loader::ParserElfHeader()
 {
-	int readSize = file.Read((uint8_t*)&(elf32.elfHeader), elf_header_size);
+	//Set elf header pointer
+	elf.header = (ELFHeader*)(elf.map);
 
-	return (elf_header_size == readSize) ? _OK : _ERR;
-}
+	//Check if it is a valid elf file
+	const uint8_t elfmagic[] = {0x7f, 'E', 'L', 'F'};
+	if (elf.header->ident[0] !=  elfmagic[0]    ) return _ERR;
+	if (elf.header->ident[1] !=  elfmagic[1]    ) return _ERR;
+	if (elf.header->ident[2] !=  elfmagic[2]    ) return _ERR;
+	if (elf.header->ident[3] !=  elfmagic[3]    ) return _ERR;
+	if (elf.header->ident[4] != _ELF_Class_32   ) return _ERR;
+	if (elf.header->type     != _ELF_Type_Rel   ) return _ERR;
+	if (elf.header->machine  != _ELF_Machine_ARM) return _ERR;
+	if (elf.header->version  != _ELF_Ver_Current) return _ERR;
 
-
-int Loader::ReadSectionHeaders()
-{
-	uint32_t sectionHeaderByteSizes = elf32.elfHeader.sectionHeaderNum * elf32.elfHeader.sectionHeaderSize;
-	uint32_t sectionHeaderOffset = elf32.elfHeader.sectionHeaderOffset;
-	
-	elf32.sectionHeaders = (SectionHeader*)malloc(sectionHeaderByteSizes);
-
-	int readSize = file.Read((uint8_t*)elf32.sectionHeaders, sectionHeaderByteSizes, sectionHeaderOffset);
-
-	return (sectionHeaderByteSizes == readSize) ? _OK : _ERR;
-}
-
-
-int Loader::ReadSymbolTableEntries()
-{
 	return _OK;
 }
 
 
-int Loader::ReadRelocationEntries()
+///Parser elf section
+int Loader::ParserElfSection()
 {
+	//Allocate sections memory
+	elf.sections = (Section*)malloc(sizeof(Section) * elf.header->sectionHeaderNum);
+	if (NULL == elf.sections) return _ERR;
+
+	//Get section headers pointer
+	SectionHeader* headers = (SectionHeader*)(elf.map + elf.header->sectionHeaderOffset);
+	//Set section header and data
+	for (uint32_t i = 0; i < elf.header->sectionHeaderNum; i++)
+	{
+		//Set header pointer
+		elf.sections[i].header = &headers[i];
+		
+		//Set data pointer
+		elf.sections[i].data = (uint8_t*)(elf.map + headers[i].offset);
+		
+		//Set the next pointer
+		if (elf.header->sectionHeaderNum != (i + 1))
+			elf.sections[i].next = (Section*)elf.sections + i + 1;
+		else
+			elf.sections[i].next = NULL;
+	}
+	
+	//Get section names pointer
+	uint8_t* name = elf.sections[elf.header->sectionHeaderStringTableIndex].data;
+	//Set section name
+	for (uint32_t i = 0; i < elf.header->sectionHeaderNum; i++)
+	{
+		elf.sections[i].name = (uint8_t*)(name + elf.sections[i].header->name);
+	}
+
+	return _OK;
+}
+
+
+///Parser ELF symbol entries
+int Loader::ParserSymbolEntries()
+{
+	//Calculate the number of symbol table
+	uint32_t symbolTableNum = 0;
+	uint32_t strTableIndex = 0;
+	for (uint32_t i = 0; i < elf.header->sectionHeaderNum; i++)
+	{
+		if (_SHT_SYMTAB == elf.sections[i].header->type) symbolTableNum++;
+
+		if (_SHT_STRTAB == elf.sections[i].header->type)
+		{
+			if (0 == strcmp(".strtab", (const char*)elf.sections[i].name))
+			{
+				strTableIndex = i;
+			}
+		}
+	}
+
+	//Allocate symbol table memory
+	elf.symTabs = (SymbolTable*)malloc(sizeof(SymbolTable) * symbolTableNum);
+	if (NULL == elf.symTabs) return _ERR;
+
+	//Set value
+	uint32_t symtabIndex = 0;
+
+	//Set symbol tables
+	for (uint32_t i = 0; i < elf.header->sectionHeaderNum; i++)
+	{
+		if (_SHT_SYMTAB == elf.sections[i].header->type)
+		{
+			//Set the symbol tables section name
+			elf.symTabs[symtabIndex].sectionName = elf.sections[i].name;
+
+			//Set the symbol tables section index
+			elf.symTabs[symtabIndex].sectionIndex = i;
+
+			//Calculate the number of symbol entries
+			uint32_t symbolNum = elf.sections[i].header->size / sizeof(SymbolEntry);
+
+			//Allocate symbols memory
+			Symbol* symbols = (Symbol*)malloc(sizeof(Symbol) * symbolNum);
+			if (NULL == symbols) return _ERR;
+			elf.symTabs[symtabIndex].symbols = symbols;
+
+			//Set symbols
+			for (uint32_t n = 0; n < symbolNum; n++)
+			{
+				//Set symbol entry
+				symbols[n].entry = elf.sections[i].symEntries + n;
+
+				//Set symbol name
+				if (_STT_SECTION == symbols[n].entry->type)
+				{
+					uint8_t* name = elf.sections[elf.header->sectionHeaderStringTableIndex].data;
+					symbols[n].name = (uint8_t*)(name + elf.sections[symbols[n].entry->index].header->name);
+				}
+				else
+				{
+					uint8_t* name = elf.sections[strTableIndex].data;
+					symbols[n].name = (uint8_t*)(name + symbols[n].entry->name);
+				}
+
+				//Set the next pointer
+				if (symbolNum != (n + 1))
+					symbols[n].next = (Symbol*)symbols + n + 1;
+				else
+					symbols[n].next = NULL;
+			}
+
+			//Set symbol table next pointer
+			if ((symtabIndex + 1) < symbolTableNum)
+				elf.symTabs[symtabIndex].next = (SymbolTable*)elf.symTabs + symtabIndex + 1;
+			else
+				elf.symTabs[symtabIndex].next = NULL;
+
+			//Update index
+			symtabIndex++;
+		}
+	}
+
+	return _OK;
+}
+
+
+///Parser ELF relocation entries
+int Loader::ParserRelocationEntries()
+{
+	//Calculate the number of relocation table
+	uint32_t relTableNum = 0;
+	for (uint32_t i = 0; i < elf.header->sectionHeaderNum; i++)
+	{
+		if (_SHT_REL == elf.sections[i].header->type) relTableNum++;
+	}
+
+	//Allocate relocation table memory
+	elf.relTabs = (RelocationTable*)malloc(sizeof(RelocationTable) * relTableNum);
+	if (NULL == elf.relTabs) return _ERR;
+
+	//Set relocation tables
+	uint32_t reltabIndex = 0;
+	for (uint32_t i = 0; i < elf.header->sectionHeaderNum; i++)
+	{
+		if (_SHT_REL == elf.sections[i].header->type)
+		{
+			//Set the relocations tables section name
+			elf.relTabs[reltabIndex].sectionName = elf.sections[i].name;
+
+			//Set the relocations tables section index
+			elf.relTabs[reltabIndex].sectionIndex = i;
+
+			//Calculate the number of relocation entries
+			uint32_t relocationNum = elf.sections[i].header->size / sizeof(RelocationEntry);
+
+			//Allocate relocations memory
+			Relocation* relocations = (Relocation*)malloc(sizeof(Relocation) * relocationNum);
+			if (NULL == relocations) return _ERR;
+			elf.relTabs[reltabIndex].relocations = relocations;
+
+			//Set relocations
+			for (uint32_t n = 0; n < relocationNum; n++)
+			{
+				//Set relocations entry
+				relocations[n].entry = elf.sections[i].relEntries + n;
+
+				//Set relocations name
+				relocations[n].name = elf.symTabs[0].symbols[relocations[n].entry->symbol].name;
+	
+				//Set the next pointer
+				if (relocationNum != (n + 1))
+					relocations[n].next = (Relocation*)relocations + n + 1;
+				else
+					relocations[n].next = NULL;
+			}
+
+			//Set relocations table next pointer
+			if ((reltabIndex + 1) < relTableNum)
+				elf.relTabs[reltabIndex].next = (RelocationTable*)elf.relTabs + reltabIndex + 1;
+			else
+				elf.relTabs[reltabIndex].next = NULL;
+
+			//Update index
+			reltabIndex++;
+		}
+	}
+
 	return _OK;
 }
 
