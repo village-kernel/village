ba433f53ce453832a561a53c42ad2d67e6f969d9
aicodeone <277579066@qq.com>
2023-03-23 12:54:56 +0800

    BSP：syscalls，sysmem，functions增加__attribute__((weak))，应该支持重载！！！！

--- BSP/ST/GCC/syscalls.c
+++ BSP/ST/GCC/syscalls.c
@@ -33,7 +33,7 @@
 
 /* Variables */
 extern int __io_putchar(int ch) __attribute__((weak));
-extern int __io_getchar(void) __attribute__((weak));
+extern int __io_getchar(void)   __attribute__((weak));
 
 
 char *__env[1] = { 0 };
@@ -41,22 +41,22 @@ char **environ = __env;
 
 
 /* Functions */
-void initialise_monitor_handles()
+__attribute__((weak)) void initialise_monitor_handles()
 {
 }
 
-int _getpid(void)
+__attribute__((weak)) int _getpid(void)
 {
 	return 1;
 }
 
-int _kill(int pid, int sig)
+__attribute__((weak)) int _kill(int pid, int sig)
 {
 	errno = EINVAL;
 	return -1;
 }
 
-void _exit (int status)
+__attribute__((weak)) void _exit (int status)
 {
 	_kill(status, -1);
 	while (1) {}		/* Make sure we hang here */
@@ -80,71 +80,70 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 	return len;
 }
 
-int _close(int file)
+__attribute__((weak)) int _close(int file)
 {
 	return -1;
 }
 
-int _fstat(int file, struct stat *st)
+__attribute__((weak)) int _fstat(int file, struct stat *st)
 {
 	st->st_mode = S_IFCHR;
 	return 0;
 }
 
-int _isatty(int file)
+__attribute__((weak)) int _isatty(int file)
 {
 	return 1;
 }
 
-int _lseek(int file, int ptr, int dir)
+__attribute__((weak)) int _lseek(int file, int ptr, int dir)
 {
 	return 0;
 }
 
-int _open(char *path, int flags, ...)
+__attribute__((weak)) int _open(char *path, int flags, ...)
 {
 	/* Pretend like we always fail */
 	return -1;
 }
 
-int _wait(int *status)
+__attribute__((weak)) int _wait(int *status)
 {
 	errno = ECHILD;
 	return -1;
 }
 
-int _unlink(char *name)
+__attribute__((weak)) int _unlink(char *name)
 {
 	errno = ENOENT;
 	return -1;
 }
 
-int _times(struct tms *buf)
+__attribute__((weak)) int _times(struct tms *buf)
 {
 	return -1;
 }
 
-int _stat(char *file, struct stat *st)
+__attribute__((weak)) int _stat(char *file, struct stat *st)
 {
 	st->st_mode = S_IFCHR;
 	return 0;
 }
 
-int _link(char *old, char *new)
+__attribute__((weak)) int _link(char *old, char *new)
 {
 	errno = EMLINK;
 	return -1;
 }
 
-int _fork(void)
+__attribute__((weak)) int _fork(void)
 {
 	errno = EAGAIN;
 	return -1;
 }
 
-int _execve(char *name, char **argv, char **env)
+__attribute__((weak)) int _execve(char *name, char **argv, char **env)
 {
 	errno = ENOMEM;
 	return -1;
 }
-
