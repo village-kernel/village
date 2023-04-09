c9bc159c66ac416be9e33237efed8e5de268a3c5
aicodeone <277579066@qq.com>
2023-04-09 22:04:06 +0800

    Templates：初步增加List模板

--- kernel/if/Templates.h
+++ kernel/if/Templates.h
@@ -43,4 +43,115 @@ typedef void (Class::*Method)(char*);
 typedef void (*Function)(char*);
 
 
+///List class template 
+template<typename Object, typename Id = int>
+class List
+{
+private:
+	//Structures
+	struct Node
+	{
+		Object* obj;
+		Node*   next;
+
+		Node(Object* obj = NULL) :
+			obj(obj),
+			next(NULL)
+		{}
+	};
+
+	//Members
+	Node* list;
+	Node* curNode;
+public:
+	///Constructor
+	List() : list(NULL) {}
+
+	///List begin node
+	void Begin() { curNode = list; }
+
+	///List next node
+	void Next() { if (NULL != curNode) curNode = curNode->next; }
+
+	///List end
+	bool End() { return (NULL == curNode); }
+
+	///List item
+	Object* Item() { return curNode->obj; }
+
+	///Add object node to list
+	void Add(Object* obj, Id id)
+	{
+		Node** nextNode = &list;
+
+		if (obj) obj->SetID(id); else return;
+
+		while (NULL != *nextNode)
+		{
+			uint32_t curModuleID = (*nextNode)->obj->GetID();
+			uint32_t newModuleID = obj->GetID();
+
+			if (newModuleID < curModuleID)
+			{
+				Node* curNode = *nextNode;
+				*nextNode = new Node(obj);
+				(*nextNode)->next = curNode;
+				return;
+			}
+			
+			nextNode = &(*nextNode)->next;
+		}
+
+		*nextNode = new Node(obj);
+	}
+
+	///Remove object node from list
+	void Remove(Object* obj, Id id)
+	{
+		Node** prevNode = &list;
+		Node** currNode = &list;
+
+		while (NULL != *currNode)
+		{
+			if (obj == (*currNode)->obj)
+			{
+				delete *currNode;
+
+				if (*prevNode == *currNode)
+					*prevNode = (*currNode)->next;
+				else
+					(*prevNode)->next = (*currNode)->next;
+
+				break;
+			}
+			else
+			{
+				prevNode = currNode;
+				currNode = &(*currNode)->next;
+			}
+		}
+	}
+
+	///List GetItem
+	Object* GetItem(Id id)
+	{
+		Node** nextNode = &list;
+
+		while (NULL != *nextNode)
+		{
+			Id curObjId = (*nextNode)->obj->GetID();
+			Id getObjId = id;
+
+			if (getObjId == curObjId)
+			{
+				return (*nextNode)->obj;
+			}
+
+			nextNode = &(*nextNode)->next;
+		}
+
+		return NULL;
+	}
+};
+ 
 #endif //!__TEMPLATES_H__
