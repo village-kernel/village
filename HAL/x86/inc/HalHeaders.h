3f4e231b1bb2de68e8f381a1448cd57b2b1443a0
aicodeone <277579066@qq.com>
2023-04-25 22:43:52 +0800

    x86：完善相关文件

---
+++ HAL/x86/inc/HalHeaders.h
@@ -0,0 +1,12 @@
+//###########################################################################
+// HalHeaders.h
+// Header file for HAL classes
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __HAL_HEADERS_H__
+#define __HAL_HEADERS_H__
+
+#include "stdint.h"
+
+#endif
