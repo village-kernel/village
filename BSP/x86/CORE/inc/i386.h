77d297bf0a0aab83ed7e28fafadd4d51d4a9e39d
aicodeone <277579066@qq.com>
2023-05-12 19:45:27 +0800

    i386：增加PIC和PIT相关定义

--- BSP/x86/CORE/inc/i386.h
+++ BSP/x86/CORE/inc/i386.h
@@ -7,6 +7,37 @@
 extern "C" {
 #endif
 
+/* PIC defines */
+#define PIC1                                 0x20             /* IO base address for master PIC */
+#define PIC2                                 0xA0             /* IO base address for slave PIC */
+#define PIC1_CMD                             PIC1
+#define PIC1_DATA                            (PIC1 + 1)
+#define PIC2_CMD                             PIC2
+#define PIC2_DATA                            (PIC2 + 1)
+#define PIC_EOI                              0x20
+
+#define PIC_READ_IRR                         0x0a            /* OCW3 irq ready next CMD read */
+#define PIC_READ_ISR                         0x0b            /* OCW3 irq service next CMD read */
+
+#define ICW1_ICW4                            0x01            /* Indicates that ICW4 will be present */
+#define ICW1_SINGLE                          0x02            /* Single (cascade) mode */
+#define ICW1_INTERVAL4                       0x04            /* Call address interval 4 (8) */
+#define ICW1_LEVEL                           0x08            /* Level triggered (edge) mode */
+#define ICW1_INIT                            0x10            /* Initialization - required! */
+ 
+#define ICW4_8086                            0x01            /* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO                            0x02            /* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE                       0x08            /* Buffered mode/slave */
+#define ICW4_BUF_MASTER                      0x0C            /* Buffered mode/master */
+#define ICW4_SFNM                            0x10            /* Special fully nested (not) */
+
+/* Timer defines */
+#define TIMER                                0x40
+#define TIMER_CH0                            (TIMER + 0)
+#define TIMER_CH1                            (TIMER + 1)
+#define TIMER_CH2                            (TIMER + 2)
+#define TIMER_CMD                            (TIMER + 3)
+
 /* IRQ numbers */
 #define IRQ_Division_By_Zero                 0
 #define IRQ_Debug                            1
@@ -44,6 +75,9 @@ extern "C" {
 #define IRQ_ATA_Channel2                     47
 
 /* i386 utils */
+#define low_8(address)   (uint8_t) ( (address) & 0xFF)
+#define high_8(address)  (uint8_t) (((address) >> 8) & 0xFF)
+
 #define low_16(address)  (uint16_t)( (address) & 0xFFFF)
 #define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
 
