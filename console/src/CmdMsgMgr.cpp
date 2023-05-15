b794875d44370d97ed48f96034e40b0668741388
aicodeone <277579066@qq.com>
2023-05-16 00:02:04 +0800

    CmdMsgMgr：优化读取输入数据逻辑，加大缓存，提高效率，解决控制台输入卡顿问题

--- console/src/CmdMsgMgr.cpp
+++ console/src/CmdMsgMgr.cpp
@@ -62,162 +62,169 @@ bool CmdMsgMgr::HandleInputData()
 {
 	if (NULL == transceiver) return false;
 
-	uint8_t byte = 0;
+	const uint8_t br_buf_size = 20;
+	uint8_t brBuff[br_buf_size] = { 0 };
+	uint8_t brSize = 0;
 
-	while (transceiver->Read(&byte, 1))
+	while ((brSize = transceiver->Read(brBuff, br_buf_size)) > 0)
 	{
-		if (rxBufPos >= arg_buffer_size)
+		for (uint8_t i = 0; i < brSize; i++)
 		{
-			rxBufPos = 0;
-			return false;
-		}
+			uint8_t byte = brBuff[i++];
 
-		switch (inputMode)
-		{
-			case _EDIT:
+			if (rxBufPos >= arg_buffer_size)
 			{
-				//ANSI ESC
-				if ('\033' == byte)
-				{
-					inputMode = _ANSI;
-				}
+				rxBufPos = 0;
+				return false;
+			}
 
-				//ASCII 32(space) ~ 126(~)
-				else if (byte >= 0x20 && byte <= 0x7e)
+			switch (inputMode)
+			{
+				case _EDIT:
 				{
-					txBuffer[txBufPos++] = byte;
-					rxBuffer[rxBufPos++] = byte;
-				}
-
-				//ASCII DEL
-				else if (0x7f == byte)
-				{
-					if (rxBufPos)
+					//ANSI ESC
+					if ('\033' == byte)
 					{
-						rxBufPos--;
+						inputMode = _ANSI;
+					}
+
+					//ASCII 32(space) ~ 126(~)
+					else if (byte >= 0x20 && byte <= 0x7e)
+					{
 						txBuffer[txBufPos++] = byte;
-					}
-				}
-
-				//ASCII CR
-				else if (0x0d == byte)
-				{
-					//String terminator
-					rxBuffer[rxBufPos++] = '\0';
-
-					//Enter and new line
-					txBuffer[txBufPos++] = '\r';
-					txBuffer[txBufPos++] = '\n';
-
-					//Check rxBufPos
-					if (rxBufPos <= 1)
-					{
-						rxBufPos = 0;
-						return false; 
+						rxBuffer[rxBufPos++] = byte;
 					}
 
-					//Set command
-					for (uint16_t i = 0; i <= rxBufPos ; i++)
+					//ASCII DEL
+					else if (0x7f == byte)
 					{
-						if ((' '  == rxBuffer[i]) ||
-							('\r' == rxBuffer[i]) ||
-							(  i  == rxBufPos   ))
+						if (rxBufPos)
 						{
-							cmdBuffer[i] = '\0'; break;
+							rxBufPos--;
+							txBuffer[txBufPos++] = byte;
+						}
+					}
+
+					//ASCII CR
+					else if (0x0d == byte)
+					{
+						//String terminator
+						rxBuffer[rxBufPos++] = '\0';
+
+						//Enter and new line
+						txBuffer[txBufPos++] = '\r';
+						txBuffer[txBufPos++] = '\n';
+
+						//Check rxBufPos
+						if (rxBufPos <= 1)
+						{
+							rxBufPos = 0;
+							return false; 
 						}
 
-						cmdBuffer[i] = rxBuffer[i];
+						//Set command
+						for (uint16_t i = 0; i <= rxBufPos ; i++)
+						{
+							if ((' '  == rxBuffer[i]) ||
+								('\r' == rxBuffer[i]) ||
+								(  i  == rxBufPos   ))
+							{
+								cmdBuffer[i] = '\0'; break;
+							}
+
+							cmdBuffer[i] = rxBuffer[i];
+						}
+
+						//Reset rxBufPos
+						rxBufPos = 0;
+						return true;
 					}
-
-					//Reset rxBufPos
-					rxBufPos = 0;
-					return true;
 				}
-			}
-			break;
+				break;
 
-			case _ANSI:
-			{
-				switch (byte)
-				{
-					case 'N': //ESC N | 0x8e | SS2 – Single Shift Two
-						break;
-					case 'O': //ESC O | 0x8f | SS3 – Single Shift Three
-						break;
-					case 'P': //ESC P | 0x90 | DCS – Device Control String
-						inputMode = _DCS;
-						break;
-					case '[': //ESC [ | 0x9b | CSI - Control Sequence Introducer
-						inputMode = _CSI;
-						break;
-					case '\\'://ESC \ | 0x9c | ST – String Terminator
-						break;
-					case ']': //ESC ] | 0x9d | OSC – Operating System Command
-						inputMode = _OSC;
-						break;
-					case 'X': //ESC X | 0x98 | SOS – Start of String
-						break;
-					case '^': //ESC ^ | 0x9e | PM – Privacy Message
-						break;
-					case '_': //ESC _ | 0x9f | APC – Application Program Command
-						break;
-					case 'c': //ESC c |      | RIS – Reset to Initial State
-						break;
-					default: break;
-				}
-			}
-			break;
-
-			case _DCS:
-			{
-				
-			}
-			break;
-
-			case _CSI:
-			{
-				//Param byte
-				if (byte >= 0x30 && byte <= 0x3f)
-				{
-
-				}
-
-				//Middle byte
-				if (byte >= 0x20 && byte <= 0x2f)
-				{
-
-				}
-
-				//Final byte
-				if (byte >= 0x40 && byte <= 0x7e)
+				case _ANSI:
 				{
 					switch (byte)
 					{
-						case 'A': //up
+						case 'N': //ESC N | 0x8e | SS2 – Single Shift Two
 							break;
-						case 'B': //down
+						case 'O': //ESC O | 0x8f | SS3 – Single Shift Three
 							break;
-						case 'C': //right
+						case 'P': //ESC P | 0x90 | DCS – Device Control String
+							inputMode = _DCS;
 							break;
-						case 'D': //left
+						case '[': //ESC [ | 0x9b | CSI - Control Sequence Introducer
+							inputMode = _CSI;
 							break;
-						case 'm': //SGR
+						case '\\'://ESC \ | 0x9c | ST – String Terminator
+							break;
+						case ']': //ESC ] | 0x9d | OSC – Operating System Command
+							inputMode = _OSC;
+							break;
+						case 'X': //ESC X | 0x98 | SOS – Start of String
+							break;
+						case '^': //ESC ^ | 0x9e | PM – Privacy Message
+							break;
+						case '_': //ESC _ | 0x9f | APC – Application Program Command
+							break;
+						case 'c': //ESC c |      | RIS – Reset to Initial State
 							break;
 						default: break;
 					}
-
-					inputMode = _EDIT;
 				}
-			}
-			break;
+				break;
 
-			case _OSC:
-			{
-				if ('\0' == byte) inputMode = _EDIT;
-			}
-			break;
+				case _DCS:
+				{
+					
+				}
+				break;
 
-			default: break;
+				case _CSI:
+				{
+					//Param byte
+					if (byte >= 0x30 && byte <= 0x3f)
+					{
+
+					}
+
+					//Middle byte
+					if (byte >= 0x20 && byte <= 0x2f)
+					{
+
+					}
+
+					//Final byte
+					if (byte >= 0x40 && byte <= 0x7e)
+					{
+						switch (byte)
+						{
+							case 'A': //up
+								break;
+							case 'B': //down
+								break;
+							case 'C': //right
+								break;
+							case 'D': //left
+								break;
+							case 'm': //SGR
+								break;
+							default: break;
+						}
+
+						inputMode = _EDIT;
+					}
+				}
+				break;
+
+				case _OSC:
+				{
+					if ('\0' == byte) inputMode = _EDIT;
+				}
+				break;
+
+				default: break;
+			}
 		}
 	}
 
