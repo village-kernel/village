93ee0b4bceaf6e66f5f37131d782b0fd8249ede6
aicodeone <277579066@qq.com>
2023-05-17 18:36:02 +0800

    stdint：增加int64_t uint64_t

--- libraries/vklibc/inc/stdint.h
+++ libraries/vklibc/inc/stdint.h
@@ -10,10 +10,12 @@
 #define int8_t          char
 #define int16_t         short
 #define int32_t         int
+#define int64_t         long int     
 
 #define uint8_t         unsigned char
 #define uint16_t        unsigned short
 #define uint32_t        unsigned int
+#define uint64_t        long unsigned int
 
 #define __ASM(code)     asm volatile(code)
 
