fdeab298423505d199acc4efd6a77ba7897700b2
aicodeone <277579066@qq.com>
2023-05-17 18:36:23 +0800

    strcat：完善逻辑

--- libraries/vklibc/string/strcat.c
+++ libraries/vklibc/string/strcat.c
@@ -13,5 +13,12 @@
 /// @return 
 char* strcat(char* s1, const char* s2)
 {
+	size_t size1 = 0;
+	size_t size2 = 0;
+
+	while(s1[size1]) { size1++; }
+	while(s2[size2]) { s1[size1++] = s2[size2++]; }
+	s1[size1] = '\0';
+
 	return s1;
 }
