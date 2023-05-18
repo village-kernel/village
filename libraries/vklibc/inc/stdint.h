940e15617cea2580d3cb8477c022347ef98a79e2
aicodeone <277579066@qq.com>
2023-05-18 14:49:45 +0800

    stdint：区分ARCH_ARM和ARCH_X86

--- libraries/vklibc/inc/stdint.h
+++ libraries/vklibc/inc/stdint.h
@@ -7,15 +7,31 @@
 #ifndef __STDINT_H__
 #define __STDINT_H__
 
+#if defined(ARCH_X86)
+
+#define int8_t          char
+#define int16_t         short
+#define int32_t         long
+#define int64_t         long int
+
+#define uint8_t         unsigned char
+#define uint16_t        unsigned short
+#define uint32_t        unsigned long
+#define uint64_t        unsigned long int
+
+#elif defined(ARCH_ARM)
+
 #define int8_t          char
 #define int16_t         short
 #define int32_t         int
-#define int64_t         long int     
+#define int64_t         long
 
 #define uint8_t         unsigned char
 #define uint16_t        unsigned short
 #define uint32_t        unsigned int
-#define uint64_t        long unsigned int
+#define uint64_t        unsigned long
+
+#endif
 
 #define __ASM(code)     asm volatile(code)
 
