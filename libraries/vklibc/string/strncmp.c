ea4959252f4e6490365f9aa74f9d6d01f651401c
aicodeone <277579066@qq.com>
2023-05-24 18:00:12 +0800

    libraries：移动到相对应的项目下面（从kernel项目去除）

--- libraries/vklibc/string/strncmp.c
+++
@@ -1,8 +0,0 @@
-//###########################################################################
-// strncmp.c
-// Header file for string classes
-//
-// $Copyright: Copyright (C) village
-//###########################################################################
-#include "string.h"
-#include "stdint.h"
