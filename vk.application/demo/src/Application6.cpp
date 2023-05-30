c203b47bf735fb8c0289dd4b3802e4453d72758f
aicodeone <277579066@qq.com>
2023-05-30 23:10:39 +0800

    vk.application：修改application为demo

--- vk.application/demo/src/Application6.cpp
+++ vk.application/demo/src/Application6.cpp
@@ -15,29 +15,29 @@ Application6::Application6()
 }
 
 
-/// @brief Read a byte from the specified port
-/// @param port 
-/// @return result
-uint8_t PortByteIn1(uint16_t port)
-{
-    uint8_t result;
-    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
-    return result;
-}
+///// @brief Read a byte from the specified port
+///// @param port 
+///// @return result
+//uint8_t PortByteIn1(uint16_t port)
+//{
+//    uint8_t result;
+//    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+//    return result;
+//}
 
-/// @brief Write a byte to the specified port
-/// @param port 
-/// @param data 
-void PortByteOut1(uint16_t port, uint8_t data)
-{
-    __asm__ volatile("out %%al, %%dx" : : "a" (data), "d" (port));
-}
+///// @brief Write a byte to the specified port
+///// @param port 
+///// @param data 
+//void PortByteOut1(uint16_t port, uint8_t data)
+//{
+//    __asm__ volatile("out %%al, %%dx" : : "a" (data), "d" (port));
+//}
 
 
 ///Check if the send register is empty
 bool IsTxRegisterEmpty()
 {
-	return (bool)(PortByteIn1(COM1 + COM_LINE_STATUS_Pos) & COM_LINE_STATUS_THRE);
+	return (bool)(PortByteIn(COM1 + COM_LINE_STATUS_Pos) & COM_LINE_STATUS_THRE);
 }
 
 ///Write data via dma.
@@ -49,7 +49,7 @@ int Write(uint8_t* data, uint32_t size, uint32_t offset)
 	{
 		if (IsTxRegisterEmpty())
 		{
-			PortByteOut1(COM1, *data++);
+			PortByteOut(COM1, *data++);
 			size--;
 		}
 	}
