86b66c7583c0d5593166f51f929b8d658daed34f
aicodeone <277579066@qq.com>
2023-08-26 12:37:07 +0800

    FatFile：增加Write方法

--- filesys/fat/src/FatFile.cpp
+++ filesys/fat/src/FatFile.cpp
@@ -26,7 +26,22 @@ FatDefs::DirEntry* FatFile::Open(const char* name, int mode)
 /// @return 
 int FatFile::Write(char* data, uint32_t size, DirEntry* entry)
 {
-	return size;
+	bool isDone = false;
+	uint32_t fileSize = entry->dir.sdir.fileSize;
+	uint32_t bytesPerSec = dbr->bpb.bytesPerSec;
+	uint32_t secPerClust = dbr->bpb.secPerClust;
+	uint32_t secSize = (fileSize + (bytesPerSec - 1)) / bytesPerSec;
+	uint32_t clusSize = (secSize + (secPerClust - 1)) / secPerClust;
+	uint32_t fstCluster = fatDisk->MergeCluster(entry->dir.sdir.fstClustHI, entry->dir.sdir.fstClustLO);
+
+	char* allocBuff = (char*)new char[clusSize * secPerClust * bytesPerSec]();
+	
+	memcpy((void*)allocBuff, (const void*)data, size);
+
+	if (clusSize == fatDisk->WriteCluster(allocBuff, clusSize, fstCluster)) isDone = true;
+
+	delete[] allocBuff;
+	return isDone ? size : 0;
 }
 
 
