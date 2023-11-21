5e8502c5859bd067548a7fc842568962c2d79169
aicodeone <277579066@qq.com>
2023-11-21 13:03:54 +0800

    vklibc++：去掉string空文件，与vklibc冲突

--- vklibc++/src/string.cpp
+++
@@ -1,8 +0,0 @@
-//###########################################################################
-// vkString.cpp
-// Definitions of the functions that string
-//
-// $Copyright: Copyright (C) village
-//###########################################################################
-#include "string.h"
-
