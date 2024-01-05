46fee910f897a779d9548c525a42c0c335da73a6
aicodeone <277579066@qq.com>
2024-01-05 22:03:08 +0800

    x86：整理driver，去除头文件，减少文件数量

--- x86/driver/src/KeyBoard.cpp
+++ x86/driver/src/KeyBoard.cpp
@@ -4,57 +4,66 @@
 //
 // $Copyright: Copyright (C) village
 //###########################################################################
-#include "KeyBoard.h"
-#include "Kernel.h"
 #include "Input.h"
+#include "Driver.h"
+#include "Kernel.h"
 #include "Hardware.h"
+#include "WorkQueue.h"
 
 
-/// @brief Constructor
-KeyBoard::KeyBoard()
+/// @brief KeyBoard
+class KeyBoard : public Driver
 {
-}
+private:
+	//Members
+	uint8_t keycode;
+	WorkQueue::Work* work;
+private:
+	/// @brief Interrupt handler
+	void InputHandler()
+	{
+		/* The PIC leaves us the scancode in port 0x60 */
+		keycode = PortByteIn(0x60);
+		workQueue.Schedule(work);
+	}
 
 
-/// @brief Deconstructor
-KeyBoard::~KeyBoard()
-{
-}
+	/// @brief Report handler
+	void ReportHandler()
+	{
+		if (keycode >= 0 && keycode <= 0x39)
+			input.ReportKey(keycode, 1);
+		else if (keycode >= 0x39 && keycode <= 0x39 + 0x80)
+			input.ReportKey(keycode - 0x80, 0);
+	}
+public:
+	/// @brief Constructor
+	KeyBoard()
+	{
+	}
 
 
-/// @brief KeyBoard Initialize
-void KeyBoard::Initialize()
-{
-	interrupt.SetISR(IRQ_Keyboard_Controller, (Method)(&KeyBoard::InputHandler), this);
-	work = workQueue.Create((Method)&KeyBoard::ReportHandler, this);
-}
+	/// @brief Deconstructor
+	~KeyBoard()
+	{
+	}
 
 
-/// @brief KeyBoard Exit
-void KeyBoard::Exit()
-{
-	interrupt.RemoveISR(IRQ_Keyboard_Controller, (Method)(&KeyBoard::InputHandler), this);
-	workQueue.Delete(work);
-}
+	/// @brief KeyBoard Initialize
+	void Initialize()
+	{
+		interrupt.SetISR(IRQ_Keyboard_Controller, (Method)(&KeyBoard::InputHandler), this);
+		work = workQueue.Create((Method)&KeyBoard::ReportHandler, this);
+	}
 
 
-/// @brief Interrupt handler
-void KeyBoard::InputHandler()
-{
-	/* The PIC leaves us the scancode in port 0x60 */
-	keycode = PortByteIn(0x60);
-	workQueue.Schedule(work);
-}
-
-
-/// @brief Report handler
-void KeyBoard::ReportHandler()
-{
-	if (keycode >= 0 && keycode <= 0x39)
-		input.ReportKey(keycode, 1);
-	else if (keycode >= 0x39 && keycode <= 0x39 + 0x80)
-		input.ReportKey(keycode - 0x80, 0);
-}
+	/// @brief KeyBoard Exit
+	void Exit()
+	{
+		interrupt.RemoveISR(IRQ_Keyboard_Controller, (Method)(&KeyBoard::InputHandler), this);
+		workQueue.Delete(work);
+	}
+};
 
 
 //Register driver
--- x86/driver/src/Mouse.cpp
+++ x86/driver/src/Mouse.cpp
@@ -4,54 +4,86 @@
 //
 // $Copyright: Copyright (C) village
 //###########################################################################
-#include "Mouse.h"
-#include "Kernel.h"
 #include "Input.h"
+#include "Driver.h"
+#include "Kernel.h"
 #include "Hardware.h"
+#include "WorkQueue.h"
 
 
-/// @brief Constructor
-Mouse::Mouse()
-	:x(0),
-	y(0)
+/// @brief Mouse
+class Mouse : public Driver
 {
-}
+private:
+	//Structures
+	union Flags
+	{
+		struct
+		{
+			uint8_t yOverflow:1;
+			uint8_t xOverflow:1;
+			uint8_t ySignBit:1;
+			uint8_t xSignBit:1;
+			uint8_t always1:1;
+			uint8_t middleBtn:1;
+			uint8_t RightBtn:1;
+			uint8_t LeftBtn:1;
+		} __attribute__((packed)) bits;
+		uint8_t byte;
+	};
+
+	//Members
+	Flags flags;
+	uint8_t x;
+	uint8_t y;
+	WorkQueue::Work* work;
+private:
+	/// @brief Mouse input handler
+	void InputHandler()
+	{
+		/* The PIC leaves us the scancode in port 0x60 */
+		flags.byte = PortByteIn(0x60);
+		x = PortByteIn(0x60);
+		y = PortByteIn(0x60);
+		workQueue.Schedule(work);
+	}
 
 
-/// @brief Deconstructor
-Mouse::~Mouse()
-{
-}
+	/// @brief Mouse report handler
+	void  ReportHandler()
+	{
+
+	}
+public:
+	/// @brief Constructor
+	Mouse()
+		:x(0),
+		y(0)
+	{
+	}
 
 
-/// @brief Mouse initialize
-void Mouse::Initialize()
-{
-	interrupt.SetISR(IRQ_Mouse_Controller, (Method)&Mouse::InputHandler, this);
-	work = workQueue.Create((Method)&Mouse::ReportHandler, this);
-}
+	/// @brief Deconstructor
+	~Mouse()
+	{
+	}
 
 
-/// @brief Mouse exit
-void Mouse::Exit()
-{
-
-}
-
-
-/// @brief Mouse input handler
-void Mouse::InputHandler()
-{
-	/* The PIC leaves us the scancode in port 0x60 */
-	flags.byte = PortByteIn(0x60);
-	x = PortByteIn(0x60);
-	y = PortByteIn(0x60);
-	workQueue.Schedule(work);
-}
+	/// @brief Mouse initialize
+	void Initialize()
+	{
+		interrupt.SetISR(IRQ_Mouse_Controller, (Method)&Mouse::InputHandler, this);
+		work = workQueue.Create((Method)&Mouse::ReportHandler, this);
+	}
 
 
-/// @brief Mouse report handler
-void  Mouse::ReportHandler()
-{
+	/// @brief Mouse exit
+	void Exit()
+	{
 
-}
+	}
+};
+
+
+//Register driver
+REGISTER_DRIVER(new Mouse(), DriverID::_miscdev, mouse);
