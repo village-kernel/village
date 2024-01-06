3ec6c18f1620f115020c9a83a0313b396f8d9299
aicodeone <277579066@qq.com>
2024-01-06 20:27:47 +0800

    CmdTasker：初步增加任务查看

---
+++ vk.application/console/cmds/CmdTasker.cpp
@@ -0,0 +1,37 @@
+//###########################################################################
+// CmdTasker.cpp
+// Definitions of the functions that manage command tasker
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#include "Cmd.h"
+#include "Console.h"
+#include "Village.h"
+
+
+/// @brief CmdTasker
+class CmdTasker : public Cmd
+{
+public:
+	/// @brief Cmd tasker execute
+	/// @param argc 
+	/// @param argv 
+	void Execute(int argc, char* argv[])
+	{
+		List<Thread::Task*> tasks = village.GetTasks();
+		for (tasks.Begin(); !tasks.IsEnd(); tasks.Next())
+		{
+			console.Output("pid %d, stack 0x%08x, psp 0x%08x, state %d, ticks %d, name %s",
+							tasks.GetNid(), 
+							tasks.Item()->stack,
+							tasks.Item()->psp,
+							tasks.Item()->state,
+							tasks.Item()->ticks,
+							tasks.GetName());
+		}
+	}
+};
+
+
+///Register cmd
+REGISTER_CMD(new CmdTasker(), tasker);
