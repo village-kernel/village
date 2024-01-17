b7f80ea24ebc5608d5944fef60d6e815a764e284
aicodeone <277579066@qq.com>
2024-01-17 15:26:23 +0800

    Input：初步完善逻辑

--- vk.kernel/core/inc/Input.h
+++ vk.kernel/core/inc/Input.h
@@ -8,19 +8,73 @@
 #define __INPUT_H__
 
 #include "Module.h"
+#include "Templates.h"
 
 /// @brief Input
 class Input : public Module
 {
+public:
+	//Structures
+	struct InputEvent
+	{
+		uint8_t keycode;
+		int     status;
+
+		InputEvent()
+			:keycode(0),
+			status(0)
+		{}
+	};
+
+	struct InputMove
+	{
+		int axisX;
+		int axisY;
+		int axisZ;
+
+		InputMove()
+			:axisX(0),
+			axisY(0),
+			axisZ(0)
+		{}
+	};
 private:
+	//Structures
+	struct Observer
+	{
+		Function    func;
+		void*       user;
+
+		Observer(Function func = NULL, void* user = NULL)
+			:func(func),
+			user(user)
+		{}
+	};
+
+	//Members
+	List<Observer*> eventObservers;
+	List<Observer*> moveObservers;
+	InputEvent      inputEvent;
+	InputMove       inputMove;
+
 	//Methods
 	Input();
 	~Input();
+	void NotifyEvent();
+	void NotifyMovement();
 public:
 	//Methods
 	void Initialize();
 	void Execute();
-	void ReportKey(uint8_t keycode, int status);
+
+	void AttachEvent(Function func, void* user = NULL);
+	void DetachEvent(Function func, void* user = NULL);
+
+	void AttachMovement(Function func, void* user = NULL);
+	void DetachMovement(Function func, void* user = NULL);
+
+	void ReportEvent(uint8_t keycode, int status);
+	void ReportMovement(int axisX, int axisY, int axisZ);
 
 	//Singleton Instance
 	static Input& Instance();
--- vk.kernel/core/src/Input.cpp
+++ vk.kernel/core/src/Input.cpp
@@ -16,7 +16,7 @@ Input::Input()
 }
 
 
-/// @brief Deconstructor
+/// @brief Destructor
 Input::~Input()
 {
 }
@@ -47,17 +47,114 @@ void Input::Initialize()
 /// @brief Input execute
 void Input::Execute()
 {
-
+	while (1) {}
 }
 
 
-/// @brief Report key
+/// @brief Attach event
+/// @param func 
+/// @param user 
+void Input::AttachEvent(Function func, void* user)
+{
+	eventObservers.Add(new Observer(func, user));
+}
+EXPORT_SYMBOL(_ZN5Input11AttachEventEPFvPvS0_ES0_);
+
+
+/// @brief Detach event
+/// @param func 
+/// @param user 
+void Input::DetachEvent(Function func, void* user)
+{
+	for (eventObservers.Begin(); !eventObservers.IsEnd(); eventObservers.Next())
+	{
+		Observer* observer = eventObservers.Item();
+
+		if ((func == observer->func) &&
+			(user == observer->user))
+		{
+			eventObservers.Remove(observer, eventObservers.GetNid());
+		}
+	}
+}
+EXPORT_SYMBOL(_ZN5Input11DetachEventEPFvPvS0_ES0_);
+
+
+/// @brief Attach movement
+/// @param func 
+/// @param user 
+void Input::AttachMovement(Function func, void* user)
+{
+	moveObservers.Add(new Observer(func, user));
+}
+EXPORT_SYMBOL(_ZN5Input14AttachMovementEPFvPvS0_ES0_);
+
+
+/// @brief Detach movement
+/// @param func 
+/// @param user 
+void Input::DetachMovement(Function func, void* user)
+{
+	for (moveObservers.Begin(); !moveObservers.IsEnd(); moveObservers.Next())
+	{
+		Observer* observer = moveObservers.Item();
+
+		if ((func == observer->func) &&
+			(user == observer->user))
+		{
+			moveObservers.Remove(observer, moveObservers.GetNid());
+		}
+	}
+}
+EXPORT_SYMBOL(_ZN5Input14DetachMovementEPFvPvS0_ES0_);
+
+
+/// @brief Notify event
+void Input::NotifyEvent()
+{
+	for (eventObservers.Begin(); !eventObservers.IsEnd(); eventObservers.Next())
+	{
+		Observer* observer = eventObservers.Item();
+		(observer->func)(observer->user, (void*)&inputEvent);
+	}
+}
+
+
+/// @brief Notify movement
+void Input::NotifyMovement()
+{
+	for (moveObservers.Begin(); !moveObservers.IsEnd(); moveObservers.Next())
+	{
+		Observer* observer = moveObservers.Item();
+		(observer->func)(observer->user, (void*)&inputMove);
+	}
+}
+
+
+/// @brief Report event
 /// @param keycode 
 /// @param status 
-void Input::ReportKey(uint8_t keycode, int status)
+void Input::ReportEvent(uint8_t keycode, int status)
 {
-	debug.Output(Debug::_Lv2, "keycode %d, keystatuts %d", keycode, status);
+	inputEvent.keycode = keycode;
+	inputEvent.status  = status;
+	NotifyEvent();
 }
+EXPORT_SYMBOL(_ZN5Input11ReportEventEhi);
+
+
+/// @brief 
+/// @param axisX 
+/// @param axisY 
+/// @param axisZ 
+void Input::ReportMovement(int axisX, int axisY, int axisZ)
+{
+	inputMove.axisX = axisX;
+	inputMove.axisY = axisY;
+	inputMove.axisZ = axisZ;
+	NotifyMovement();
+}
+EXPORT_SYMBOL(_ZN5Input14ReportMovementEiii);
 
 
 ///Register module
