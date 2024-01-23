3b6c879db139a6a95b2026a30d428f5212aa8e63
aicodeone <277579066@qq.com>
2024-01-23 17:31:15 +0800

    Input：还是老方式比较适合，标准观察者模式效率不高也不适用，观察者自己的Update接口得为public，但有些情况是不想为public。还有内核层到应用层之间是有隔阂的，执行效率底下，input考虑高效率，故不使用标准观察者模式。

--- vk.kernel/subsys/inc/Input.h
+++ vk.kernel/subsys/inc/Input.h
@@ -10,30 +10,64 @@
 #include "Module.h"
 #include "Templates.h"
 
-
-/// @brief Input observer
-class InputObserver
-{
-public:
-	//Methods
-	//virtual ~InputObserver() {}
-	virtual void Update() = 0;
-};
-
-
 /// @brief Input
 class Input : public Module
 {
-protected:
-	//Members
-	uint8_t keycode;
-	int     status;
-	int     axisX;
-	int     axisY;
-	int     axisZ;
+public:
+	//Enumerations
+	enum Type
+	{
+		_Event = 0,
+		_MoveMent,
+		_AllType,
+	};
+
+	//Structures
+	struct InputEvent
+	{
+		uint8_t keycode;
+		int     status;
+
+		InputEvent()
+			:keycode(0),
+			status(0)
+		{}
+	};
+
+	struct InputMove
+	{
+		int axisX;
+		int axisY;
+		int axisZ;
+
+		InputMove()
+			:axisX(0),
+			axisY(0),
+			axisZ(0)
+		{}
+	};
+private:
+	//Structures
+	struct Observer
+	{
+		Function    func;
+		void*       user;
+
+		Observer(Function func = NULL, void* user = NULL)
+			:func(func),
+			user(user)
+		{}
+	};
 
 	//Members
-	List<InputObserver*> observers;
+	List<Observer*> observers[_AllType];
+	InputEvent      inputEvent;
+	InputMove       inputMove;
+
+	//Methods
+	void Attach(List<Observer*>& observer, Function func, void* user = NULL);
+	void Detach(List<Observer*>& observer, Function func, void* user = NULL);
+	void Notify(List<Observer*>& observer, void* argv);
 public:
 	//Methods
 	Input();
@@ -41,15 +75,12 @@ public:
 	void Initialize();
 	void Execute();
 
-	void Attach(InputObserver* observer);
-	void Detach(InputObserver* observer);
-	void Notify();
-
+	void Attach(Type type, Method method, Class* user);
+	void Attach(Type type, Function func, void* user = NULL);
+	void Detach(Type type, Method method, Class* user);
+	void Detach(Type type, Function func, void* user = NULL);
 	void ReportEvent(uint8_t keycode, int status);
-	void ReadEvent(uint8_t& keycode, int& status);
-
 	void ReportMovement(int axisX, int axisY, int axisZ);
-	void ReadMovement(int& axisX, int& axisY, int& axisZ);
 };
 
 #endif //!__INPUT_H__
--- vk.kernel/subsys/src/Input.cpp
+++ vk.kernel/subsys/src/Input.cpp
@@ -34,81 +34,112 @@ void Input::Execute()
 }
 
 
-/// @brief 
+/// @brief Input base attach
 /// @param observer 
-void Input::Attach(InputObserver* observer)
+/// @param func 
+/// @param user 
+void Input::Attach(List<Observer*>& observers, Function func, void* user)
 {
-	observers.Add(observer);
+	observers.Add(new Observer(func, user));
 }
-EXPORT_SYMBOL(_ZN5Input6AttachEP13InputObserver);
 
 
-/// @brief 
+/// @brief Input base detach
 /// @param observer 
-void Input::Detach(InputObserver* observer)
-{
-	observers.Remove(observer);
-}
-EXPORT_SYMBOL(_ZN5Input6DetachEP13InputObserver);
-
-
-/// @brief 
-void Input::Notify()
+/// @param func 
+/// @param user 
+void Input::Detach(List<Observer*>& observers, Function func, void* user)
 {
 	for (observers.Begin(); !observers.IsEnd(); observers.Next())
 	{
-		observers.Item()->Update();
+		Observer* observer = observers.Item();
+
+		if ((func == observer->func) &&
+			(user == observer->user))
+		{
+			observers.Remove(observer, observers.GetNid());
+		}
 	}
 }
-EXPORT_SYMBOL(_ZN5Input6NotifyEv);
 
 
-/// @brief 
+/// @brief Input base notify
+/// @param observer 
+/// @param argv 
+void Input::Notify(List<Observer*>& observers, void* argv)
+{
+	for (observers.Begin(); !observers.IsEnd(); observers.Next())
+	{
+		Observer* observer = observers.Item();
+		(observer->func)(observer->user, argv);
+	}
+}
+
+
+/// @brief Input attach
+/// @param method 
+/// @param user 
+void Input::Attach(Type type, Method method, Class* user)
+{
+	Attach(observers[type], union_cast<Function>(method), (void*)user);
+}
+EXPORT_SYMBOL(_ZN5Input6AttachENS_4TypeEM5ClassFvPvEPS1_);
+
+
+/// @brief Input Attach
+/// @param func 
+/// @param user 
+void Input::Attach(Type type, Function func, void* user)
+{
+	Attach(observers[type], func, user);
+}
+EXPORT_SYMBOL(_ZN5Input6AttachENS_4TypeEPFvPvS1_ES1_);
+
+
+/// @brief Input Detach
+/// @param method 
+/// @param user 
+void Input::Detach(Type type, Method method, Class* user)
+{
+	Detach(observers[type], union_cast<Function>(method), (void*)user);
+}
+EXPORT_SYMBOL(_ZN5Input6DetachENS_4TypeEM5ClassFvPvEPS1_);
+
+
+/// @brief Input Detach
+/// @param func 
+/// @param user 
+void Input::Detach(Type type, Function func, void* user)
+{
+	Detach(observers[type], func, user);
+}
+EXPORT_SYMBOL(_ZN5Input6DetachENS_4TypeEPFvPvS1_ES1_);
+
+
+/// @brief Input report event
 /// @param keycode 
 /// @param status 
 void Input::ReportEvent(uint8_t keycode, int status)
 {
-	this->keycode = keycode;
-	this->status  = status;
+	inputEvent.keycode = keycode;
+	inputEvent.status  = status;
+	Notify(observers[_Event], (void*)&inputEvent);
 }
 EXPORT_SYMBOL(_ZN5Input11ReportEventEhi);
 
 
-/// @brief 
-/// @param keycode 
-/// @param status 
-void Input::ReadEvent(uint8_t& keycode, int& status)
-{
-	keycode = this->keycode;
-	status  = this->status;
-}
-EXPORT_SYMBOL(_ZN5Input9ReadEventERhRi);
-
-
-/// @brief 
+/// @brief Input report movement
 /// @param axisX 
 /// @param axisY 
 /// @param axisZ 
 void Input::ReportMovement(int axisX, int axisY, int axisZ)
 {
-	this->axisX = axisX;
-	this->axisY = axisY;
-	this->axisZ = axisZ;
+	inputMove.axisX = axisX;
+	inputMove.axisY = axisY;
+	inputMove.axisZ = axisZ;
+	Notify(observers[_MoveMent], (void*)&inputMove);
 }
 EXPORT_SYMBOL(_ZN5Input14ReportMovementEiii);
-
-
-/// @brief 
-/// @param axisX 
-/// @param axisY 
-/// @param axisZ 
-void Input::ReadMovement(int& axisX, int& axisY, int& axisZ)
-{
-	axisX = this->axisX;
-	axisY = this->axisY;
-	axisZ = this->axisZ;
-}
-EXPORT_SYMBOL(_ZN5Input12ReadMovementERiS0_S0_);
 
 
 ///Register module
