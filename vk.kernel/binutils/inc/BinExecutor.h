eddd448e47a9f60006cb7f6503ed5bce25cd4376
aicodeone <277579066@qq.com>
2024-01-27 15:51:44 +0800

    BinExecutor：增加bin执行器

---
+++ vk.kernel/binutils/inc/BinExecutor.h
@@ -0,0 +1,26 @@
+//###########################################################################
+// BinExecutor.h
+// Declarations of the functions that manage bin executor
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __BIN_EXECUTOR_H__
+#define __BIN_EXECUTOR_H__
+
+#include "BaseExecutor.h"
+#include "BinLoader.h"
+
+
+/// @brief BinExecutor
+class BinExecutor : public BaseExecutor, public Class
+{
+private:
+	//Members
+	BinLoader bin;
+private:
+	//Methods
+	int Execute(const char* path);
+	void Sandbox();
+};
+
+#endif //!__BIN_EXECUTOR_H__
---
+++ vk.kernel/binutils/src/BinExecutor.cpp
@@ -0,0 +1,29 @@
+//###########################################################################
+// BinExecutor.cpp
+// Definitions of the functions that manage bin executor
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#include "Kernel.h"
+#include "BinExecutor.h"
+
+
+/// @brief BinExecutor Execute
+/// @param path 
+/// @return pid
+int BinExecutor::Execute(const char* path)
+{
+	//Load, parser and execute bin file
+	if (bin.Load(path) != Result::_OK) return 0;
+
+	//Create a sandboxed thread to run the app
+	return Kernel::thread.CreateTask(path, (Method)&BinExecutor::Sandbox, this);
+}
+
+
+/// @brief BinExecutor execute app
+void BinExecutor::Sandbox()
+{
+	bin.Execute(argc, argv);
+	bin.Exit();
+}
