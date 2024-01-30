1a8c4bdd4f5d07a2ce69d232443ab620de38c308
aicodeone <277579066@qq.com>
2024-01-30 15:47:19 +0800

    Input：修改Input实现方式

--- vk.kernel/subsys/src/Input.cpp
+++ vk.kernel/subsys/src/Input.cpp
@@ -6,127 +6,143 @@
 //###########################################################################
 #include "Input.h"
 #include "Kernel.h"
+#include "Templates.h"
 
 
-/// @brief Constructor
-Input::Input()
+/// @brief ConcreteInput
+class ConcreteInput : public Input
 {
-}
-
-
-/// @brief Destructor
-Input::~Input()
-{
-}
-
-
-/// @brief Input base attach
-/// @param observer 
-/// @param func 
-/// @param user 
-void Input::Attach(List<Observer*>& observers, Function func, void* user)
-{
-	observers.Add(new Observer(func, user));
-}
-
-
-/// @brief Input base detach
-/// @param observer 
-/// @param func 
-/// @param user 
-void Input::Detach(List<Observer*>& observers, Function func, void* user)
-{
-	for (observers.Begin(); !observers.IsEnd(); observers.Next())
+private:
+	//Structures
+	struct Observer
 	{
-		Observer* observer = observers.Item();
+		Function    func;
+		void*       user;
 
-		if ((func == observer->func) &&
-			(user == observer->user))
+		Observer(Function func = NULL, void* user = NULL)
+			:func(func),
+			user(user)
+		{}
+	};
+
+	//Members
+	List<Observer*> observers[_AllType];
+	Event           event;
+	Movement        movement;
+private:
+	/// @brief Input base attach
+	/// @param observer 
+	/// @param func 
+	/// @param user 
+	void Attach(List<Observer*>& observers, Function func, void* user)
+	{
+		observers.Add(new Observer(func, user));
+	}
+
+
+	/// @brief Input base detach
+	/// @param observer 
+	/// @param func 
+	/// @param user 
+	void Detach(List<Observer*>& observers, Function func, void* user)
+	{
+		for (observers.Begin(); !observers.IsEnd(); observers.Next())
 		{
-			observers.Remove(observer, observers.GetNid());
+			Observer* observer = observers.Item();
+
+			if ((func == observer->func) &&
+				(user == observer->user))
+			{
+				observers.Remove(observer, observers.GetNid());
+			}
 		}
 	}
-}
 
 
-/// @brief Input base notify
-/// @param observer 
-/// @param argv 
-void Input::Notify(List<Observer*>& observers, void* argv)
-{
-	for (observers.Begin(); !observers.IsEnd(); observers.Next())
+	/// @brief Input base notify
+	/// @param observer 
+	/// @param argv 
+	void Notify(List<Observer*>& observers, void* argv)
 	{
-		Observer* observer = observers.Item();
-		(observer->func)(observer->user, argv);
+		for (observers.Begin(); !observers.IsEnd(); observers.Next())
+		{
+			Observer* observer = observers.Item();
+			(observer->func)(observer->user, argv);
+		}
 	}
-}
+public:
+	/// @brief Constructor
+	Input()
+	{
+	}
 
 
-/// @brief Input attach
-/// @param method 
-/// @param user 
-void Input::Attach(Type type, Method method, Class* user)
-{
-	Attach(observers[type], union_cast<Function>(method), (void*)user);
-}
-EXPORT_SYMBOL(_ZN5Input6AttachENS_4TypeEM5ClassFvPvEPS1_);
+	/// @brief Destructor
+	~Input()
+	{
+	}
 
 
-/// @brief Input Attach
-/// @param func 
-/// @param user 
-void Input::Attach(Type type, Function func, void* user)
-{
-	Attach(observers[type], func, user);
-}
-EXPORT_SYMBOL(_ZN5Input6AttachENS_4TypeEPFvPvS1_ES1_);
+	/// @brief Input attach
+	/// @param method 
+	/// @param user 
+	void Attach(Type type, Method method, Class* user)
+	{
+		Attach(observers[type], union_cast<Function>(method), (void*)user);
+	}
 
 
-/// @brief Input Detach
-/// @param method 
-/// @param user 
-void Input::Detach(Type type, Method method, Class* user)
-{
-	Detach(observers[type], union_cast<Function>(method), (void*)user);
-}
-EXPORT_SYMBOL(_ZN5Input6DetachENS_4TypeEM5ClassFvPvEPS1_);
+	/// @brief Input Attach
+	/// @param func 
+	/// @param user 
+	void Attach(Type type, Function func, void* user)
+	{
+		Attach(observers[type], func, user);
+	}
 
 
-/// @brief Input Detach
-/// @param func 
-/// @param user 
-void Input::Detach(Type type, Function func, void* user)
-{
-	Detach(observers[type], func, user);
-}
-EXPORT_SYMBOL(_ZN5Input6DetachENS_4TypeEPFvPvS1_ES1_);
+	/// @brief Input Detach
+	/// @param method 
+	/// @param user 
+	void Detach(Type type, Method method, Class* user)
+	{
+		Detach(observers[type], union_cast<Function>(method), (void*)user);
+	}
 
 
-/// @brief Input report event
-/// @param keycode 
-/// @param status 
-void Input::ReportEvent(uint8_t keycode, int status)
-{
-	inputEvent.keycode = keycode;
-	inputEvent.status  = status;
-	Notify(observers[_Event], (void*)&inputEvent);
-}
-EXPORT_SYMBOL(_ZN5Input11ReportEventEhi);
+	/// @brief Input Detach
+	/// @param func 
+	/// @param user 
+	void Detach(Type type, Function func, void* user)
+	{
+		Detach(observers[type], func, user);
+	}
 
 
-/// @brief Input report movement
-/// @param axisX 
-/// @param axisY 
-/// @param axisZ 
-void Input::ReportMovement(int axisX, int axisY, int axisZ)
-{
-	inputMove.axisX = axisX;
-	inputMove.axisY = axisY;
-	inputMove.axisZ = axisZ;
-	Notify(observers[_MoveMent], (void*)&inputMove);
-}
-EXPORT_SYMBOL(_ZN5Input14ReportMovementEiii);
+	/// @brief Input report event
+	/// @param keycode 
+	/// @param status 
+	void ReportEvent(int keycode, int status)
+	{
+		event.keycode = keycode;
+		event.status  = status;
+		Notify(observers[_Event], (void*)&event);
+	}
+
+
+	/// @brief Input report movement
+	/// @param axisX 
+	/// @param axisY 
+	/// @param axisZ 
+	void ReportMovement(int axisX, int axisY, int axisZ)
+	{
+		movement.axisX = axisX;
+		movement.axisY = axisY;
+		movement.axisZ = axisZ;
+		Notify(observers[_Movement], (void*)&movement);
+	}
+};
 
 
 ///Register module
-REGISTER_MODULE(new Input(), ModuleID::_input, input);
+REGISTER_MODULE(new ConcreteInput(), ModuleID::_input, input);
