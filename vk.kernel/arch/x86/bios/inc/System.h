281ac30b05073fda00b2cd0b9c3bb285d6453b1f
aicodeone <277579066@qq.com>
2024-02-03 13:30:05 +0800

    System：更改实现方式

--- vk.kernel/arch/x86/bios/inc/System.h
+++ vk.kernel/arch/x86/bios/inc/System.h
@@ -8,48 +8,17 @@
 #define __SYSTEM_H__
 
 #include "Hardware.h"
+#include "Module.h"
 
-///System
-class System
+/// @brief System
+class System : public Module
 {
-private:
-	static uint32_t sysTicks;
 public:
-	static void Initialize();
-	static void ConfigureMPU();
-	static void ConfigurePower();
-	static void ConfigureClock();
-
-	///Reset x86 core
-	static inline void Reboot()
-	{
-		uint64_t null_idtr = 0;
-		__asm volatile ("lidt %0; int3" :: "m" (null_idtr));
-
-		//Loop with interrupt off if it doesn't work
-		__asm volatile ("cli");
-		while (1) { __asm volatile ("hlt"); }
-	}
-
-	///Delays for a specified number of milliseconds.
-	static inline void DelayMs(uint32_t millis)
-	{
-		uint32_t delayStart = sysTicks;
-		uint32_t delayCycles = millis;
-		while ((sysTicks - delayStart) < delayCycles);
-	}
-
-	///Get system clock count
-	static inline uint32_t GetSysClkCounts() { return sysTicks; }
-
-	///System clock counter
-	static inline void SysTickCounter() { sysTicks++; }
-
-	///Enables IRQ interrupts
-	static inline void EnableIRQ() { __asm volatile("sti"); }
-
-	///Disables IRQ interrupts
-	static inline void DisableIRQ() { __asm volatile("cli"); }
+	virtual uint32_t GetSysClkCounts() = 0;
+	virtual void DelayMs(uint32_t millis) = 0;
+	virtual void EnableIRQ() = 0;
+	virtual void DisableIRQ() = 0;
+	virtual void Reboot() = 0;
 };
 
 #endif //!__SYSTEM_H__
