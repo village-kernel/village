cb3aa1bb2108600c563ecabee466ea400a51519c
aicodeone <277579066@qq.com>
2024-02-03 13:38:34 +0800

    Thread：更改实现方式

--- vk.kernel/core/src/Thread.cpp
+++ vk.kernel/core/src/Thread.cpp
@@ -7,202 +7,221 @@
 #include "Thread.h"
 #include "System.h"
 #include "Kernel.h"
+#include "Memory.h"
+#include "System.h"
+#include "Scheduler.h"
 
 
-/// @brief Constructor
-Thread::Thread()
+/// @brief ConcreteThread
+class ConcreteThread : public Thread
 {
-}
-
-
-/// @brief Fini constructor
-Thread::~Thread()
-{
-}
-
-
-/// @brief Thread Initialize
-void Thread::Initialize()
-{
-	//Frist task should be idle task and the pid is 0
-	CreateTask("IdleTask", (Method)&Thread::IdleTask, this);
-
-	//Set first node
-	tasks.Begin();
-}
-
-
-/// @brief Thread execute
-void Thread::Execute()
-{
-	TaskHandler(union_cast<Function>(&Thread::IdleTask), (void*)this);
-}
-
-
-/// @brief Create new task
-/// @param function task execute function
-/// @param user task execute uesr
-/// @param args task execute args
-/// @return pid
-int Thread::CreateTask(const char* name, Function function, void* user, void* args)
-{
-	//Create a new task and allocate stack space
-	Task* task = new Task(Kernel::memory.StackAlloc(task_stack_size));
+private:
+	//Static constants
+	static const uint32_t task_stack_size = TASK_STACK;
+	static const uint32_t psp_frame_size = sizeof(TaskContext) >> 2;
 	
-	//Check whether stack allocation is successful
-	if (NULL == task && 0 == task->stack) return -1;
-
-	//Fill the stack content
-	task->psp = task->stack - psp_frame_size;
-	*(TaskContext*)task->psp = TaskContext
-	(
-		(uint32_t)union_cast<uint32_t>(&Thread::TaskHandler),
-		(uint32_t)this,
-		(uint32_t)function,
-		(uint32_t)user,
-		(uint32_t)args
-	);
-
-	return tasks.Add(task, (char*)name);
-}
-EXPORT_SYMBOL(_ZN6Thread10CreateTaskEPKcPFvPvS2_ES2_S2_);
-
-
-/// @brief Create new task for cpp
-/// @param method task execute class method
-/// @param user task execute class
-/// @param args task execute args
-/// @return pid
-int Thread::CreateTask(const char* name, Method method, Class *user, void* args)
-{
-	return CreateTask(name, union_cast<Function>(method), (void*)user, args);
-}
-EXPORT_SYMBOL(_ZN6Thread10CreateTaskEPKcM5ClassFvPvEPS2_S3_);
-
-
-/// @brief Thread delete task
-/// @param pid process id
-/// @return result
-int Thread::DeleteTask(int pid)
-{
-	Task* task = tasks.GetItem(pid);
-	Kernel::memory.Free(task->stack);
-	return tasks.Remove(task, pid);
-}
-EXPORT_SYMBOL(_ZN6Thread10DeleteTaskEi);
-
-
-/// @brief Thread wait for task
-/// @param pid process id
-/// @return result
-int Thread::WaitForTask(int pid)
-{
-	//Gets the task
-	Task* task = tasks.GetItem(pid);
-
-	//Check task is valid
-	if (NULL != task)
+	//Members
+	List<Task*> tasks;
+	Memory*     memory;
+	System*     system;
+	Scheduler*  scheduler;
+private:
+	/// @brief Thread task function handler 
+	/// @param function execute function
+	/// @param user execute user
+	void TaskHandler(Function function, void* user, void* args)
 	{
-		//Blocking wait
-		while(task->state != TaskState::Exited) {}
-		return _OK;
-	}
-	return _ERR;
-}
-EXPORT_SYMBOL(_ZN6Thread11WaitForTaskEi);
-
-
-/// @brief Get tasks
-/// @return 
-List<Thread::Task*> Thread::GetTasks()
-{
-	return tasks;
-}
-EXPORT_SYMBOL(_ZN6Thread8GetTasksEv);
-
-
-/// @brief Thread sleep
-/// @param ticks sleep ticks
-void Thread::Sleep(uint32_t ticks)
-{
-	if(tasks.GetNid() > 0)
-	{
-		tasks.Item()->state = TaskState::Suspend;
-		tasks.Item()->ticks = System::GetSysClkCounts() + ticks;
-		Kernel::scheduler.Rescheduler(Scheduler::Unprivileged);
-	}
-}
-EXPORT_SYMBOL(_ZN6Thread5SleepEm);
-
-
-/// @brief Thread Exit
-void Thread::Exit()
-{
-	if(tasks.GetNid() > 0)
-	{
-		tasks.Item()->state = TaskState::Exited;
-		DeleteTask(tasks.GetNid());
-		Kernel::scheduler.Rescheduler(Scheduler::Unprivileged);
-	}
-}
-EXPORT_SYMBOL(_ZN6Thread4ExitEv);
-
-
-/// @brief Idle task
-#if defined(ARCH_X86)
-void Thread::IdleTask() { while (1) { __asm volatile ("hlt" ::: "memory"); }}
-#else
-void Thread::IdleTask() { while (1) { __asm volatile("NOP"); } }
-#endif
-
-
-/// @brief Thread task function handler 
-/// @param function execute function
-/// @param user execute user
-void Thread::TaskHandler(Function function, void* user, void* args)
-{
-	if (NULL != function)
-	{
-		(function)(user, args);
-	}
-	Kernel::thread.Exit();
-}
-
-
-/// @brief Save task PSP
-/// @param psp process stack pointer
-void Thread::SaveTaskPSP(uint32_t psp)
-{
-	tasks.Item()->psp = psp;
-}
-
-
-/// @brief Get current task psp
-/// @return process stack pointer
-uint32_t Thread::GetTaskPSP()
-{
-	return tasks.Item()->psp;
-}
-
-
-/// @brief Select next task, round-Robin scheduler
-void Thread::SelectNextTask()
-{
-	while (1)
-	{
-		//Set next task as current task
-		tasks.Next(); if (tasks.IsEnd()) tasks.Begin();
-
-		//Check current task state
-		if (TaskState::Suspend == tasks.Item()->state)
+		if (NULL != function)
 		{
-			if(System::GetSysClkCounts() >= tasks.Item()->ticks)
-			{
-				tasks.Item()->state = TaskState::Running;
-			}
+			(function)(user, args);
 		}
-
-		//Break when task state is running
-		if (TaskState::Running == tasks.Item()->state) break;
+		Exit();
 	}
-}
+public:
+	/// @brief Constructor
+	ConcreteThread()
+		:memory(NULL),
+		system(NULL),
+		scheduler(NULL)
+	{
+	}
+
+
+	/// @brief Destructor
+	~ConcreteThread()
+	{
+	}
+
+
+	/// @brief Thread Initialize
+	void Initialize()
+	{
+		//Gets the memory pointer
+		memory = (Memory*)kernel->modular->GetModule(ModuleID::_memory);
+
+		//Gets the system pointer
+		system = (System*)kernel->modular->GetModule(ModuleID::_system);
+
+		//Gets the scheduler pointer
+		scheduler = (Scheduler*)kernel->modular->GetModule(ModuleID::_scheduler);
+
+		//Frist task should be idle task and the pid is 0
+		CreateTask("IdleTask", (Method)&ConcreteThread::IdleTask, this);
+
+		//Set first node
+		tasks.Begin();
+	}
+
+
+	/// @brief Create new task
+	/// @param function task execute function
+	/// @param user task execute uesr
+	/// @param args task execute args
+	/// @return pid
+	int CreateTask(const char* name, Function function, void* user = NULL, void* args = NULL)
+	{
+		//Create a new task and allocate stack space
+		Task* task = new Task(memory->StackAlloc(task_stack_size));
+		
+		//Check whether stack allocation is successful
+		if (NULL == task && 0 == task->stack) return -1;
+
+		//Fill the stack content
+		task->psp = task->stack - psp_frame_size;
+		*(TaskContext*)task->psp = TaskContext
+		(
+			(uint32_t)union_cast<uint32_t>(&ConcreteThread::TaskHandler),
+			(uint32_t)this,
+			(uint32_t)function,
+			(uint32_t)user,
+			(uint32_t)args
+		);
+
+		return tasks.Add(task, (char*)name);
+	}
+
+
+	/// @brief Create new task for cpp
+	/// @param method task execute class method
+	/// @param user task execute class
+	/// @param args task execute args
+	/// @return pid
+	int CreateTask(const char* name, Method method, Class *user, void* args = NULL)
+	{
+		return CreateTask(name, union_cast<Function>(method), (void*)user, args);
+	}
+
+
+	/// @brief Thread delete task
+	/// @param pid process id
+	/// @return result
+	int DeleteTask(int pid)
+	{
+		Task* task = tasks.GetItem(pid);
+		memory->Free(task->stack);
+		return tasks.Remove(task, pid);
+	}
+
+
+	/// @brief Thread wait for task
+	/// @param pid process id
+	/// @return result
+	int WaitForTask(int pid)
+	{
+		//Gets the task
+		Task* task = tasks.GetItem(pid);
+
+		//Check task is valid
+		if (NULL != task)
+		{
+			//Blocking wait
+			while(task->state != TaskState::Exited) {}
+			return _OK;
+		}
+		return _ERR;
+	}
+
+
+	/// @brief Get tasks
+	/// @return 
+	List<Thread::Task*> GetTasks()
+	{
+		return tasks;
+	}
+
+
+	/// @brief Thread sleep
+	/// @param ticks sleep ticks
+	void Sleep(uint32_t ticks)
+	{
+		if(tasks.GetNid() > 0)
+		{
+			tasks.Item()->state = TaskState::Suspend;
+			tasks.Item()->ticks = system->GetSysClkCounts() + ticks;
+			scheduler->Rescheduler(Scheduler::Unprivileged);
+		}
+	}
+
+
+	/// @brief Thread Exit
+	void Exit()
+	{
+		if(tasks.GetNid() > 0)
+		{
+			tasks.Item()->state = TaskState::Exited;
+			DeleteTask(tasks.GetNid());
+			scheduler->Rescheduler(Scheduler::Unprivileged);
+		}
+	}
+
+
+	/// @brief Save task PSP
+	/// @param psp process stack pointer
+	void SaveTaskPSP(uint32_t psp)
+	{
+		tasks.Item()->psp = psp;
+	}
+
+
+	/// @brief Get current task psp
+	/// @return process stack pointer
+	uint32_t GetTaskPSP()
+	{
+		return tasks.Item()->psp;
+	}
+
+
+	/// @brief Select next task, round-Robin scheduler
+	void SelectNextTask()
+	{
+		while (1)
+		{
+			//Set next task as current task
+			tasks.Next(); if (tasks.IsEnd()) tasks.Begin();
+
+			//Check current task state
+			if (TaskState::Suspend == tasks.Item()->state)
+			{
+				if(system->GetSysClkCounts() >= tasks.Item()->ticks)
+				{
+					tasks.Item()->state = TaskState::Running;
+				}
+			}
+
+			//Break when task state is running
+			if (TaskState::Running == tasks.Item()->state) break;
+		}
+	}
+
+
+	/// @brief Idle task
+	#if defined(ARCH_X86)
+	void IdleTask() { while (1) { __asm volatile ("hlt" ::: "memory"); }}
+	#else
+	void IdleTask() { while (1) { __asm volatile("NOP"); } }
+	#endif
+};
+
+
+///Register module
+REGISTER_MODULE(ConcreteThread, ModuleID::_thread, thread);
