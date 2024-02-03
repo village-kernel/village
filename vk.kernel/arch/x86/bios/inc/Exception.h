748bd38bca2b8d4d5f4c104058498905f609624d
aicodeone <277579066@qq.com>
2024-02-03 13:29:35 +0800

    Exception：更改实现方式

--- vk.kernel/arch/x86/bios/inc/Exception.h
+++ vk.kernel/arch/x86/bios/inc/Exception.h
@@ -7,38 +7,13 @@
 #ifndef __EXCEPTION_H__
 #define __EXCEPTION_H__
 
-#include "Templates.h"
+#include "Module.h"
 
 
 /// @brief Exception
-class Exception : public Class
+class Exception : public Module
 {
-private:
-	//Methods
-	void DivisionByZeroHandler();
-	void DebugHandler();
-	void NonMaskableInterruptHandler();
-	void BreakpointHandler();
-	void IntoDetectedOverflowHandler();
-	void OutOfBoundsHandler();
-	void InvalidOpcodeHandler();
-	void NoCoprocessorHandler();
-	void DouleFaultHandler();
-	void CoprocessorSegmentOverrunHandler();
-	void BadTSSHandler();
-	void SegmentNotPresentHandler();
-	void StackFaultHandler();
-	void GeneralProtectionFaultHandler();
-	void PageFaultHandler();
-	void UnknownInterruptHandler();
-	void CoprocessorFaultHandler();
-	void AlignmentCheckHandler();
-	void MachineCheckHandler();
-public:
-	//Methods
-	Exception();
-	~Exception();
-	void Initialize();
+
 };
 
 #endif //!__EXCEPTION_H__
