2541b48363dd1c43edb733a1c8e8b62e5bc35fd3
aicodeone <277579066@qq.com>
2024-02-04 17:33:12 +0800

    libcinit：增加libc初始化

---
+++ vk.hardware/arm/cortex-m/mcu/st/GCC/libcinit.c
@@ -0,0 +1,41 @@
+
+/// @brief execute preinit_arrary
+/// @param  
+void __libc_preinit_arrary(void)
+{
+	extern void (*__preinit_array_start []) (void);
+	extern void (*__preinit_array_end   []) (void);
+
+	int count = __preinit_array_end - __preinit_array_start;
+	for (int i = 0; i < count; i++)
+		__preinit_array_start[i]();
+}
+
+
+/// @brief execute init_arrary
+/// @param  
+void __libc_init_array(void)
+{
+	extern void (*__init_array_start []) (void);
+	extern void (*__init_array_end   []) (void);
+
+	int count = __init_array_end - __init_array_start;
+	for (int i = 0; i < count; i++)
+		__init_array_start[i]();
+}
+
+
+/// @brief execute fini_arrary
+/// @param  
+void __libc_fini_array(void)
+{
+	extern void (*__fini_array_start []) (void);
+	extern void (*__fini_array_end   []) (void);
+
+	int count = __fini_array_end - __fini_array_start;
+	
+	for (int i = 0; i < count; i++)
+	{
+		__fini_array_start[i]();
+	}
+}
