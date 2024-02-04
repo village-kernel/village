b5a98be9a906360a72a8605989675217e4102d78
aicodeone <277579066@qq.com>
2024-02-04 17:31:58 +0800

    System：更改实现方式

---
+++ vk.kernel/arch/arm/cortex-m/inc/System.h
@@ -0,0 +1,25 @@
+//###########################################################################
+// System.h
+// A mostly static class containing system wide functions.
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __SYSTEM_H__
+#define __SYSTEM_H__
+
+#include "Module.h"
+
+
+/// @brief System
+class System : public Module
+{
+public:
+	virtual void SysTickCounter() = 0;
+	virtual uint32_t GetSysClkCounts() = 0;
+	virtual void DelayMs(uint32_t millis) = 0;
+	virtual void EnableIRQ() = 0;
+	virtual void DisableIRQ() = 0;
+	virtual void Reboot() = 0;
+};
+
+#endif //!__SYSTEM_H__
