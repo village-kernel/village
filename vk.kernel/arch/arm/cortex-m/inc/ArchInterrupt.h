899a0d4e6ba646676d70a90630c7ba91a853c286
aicodeone <277579066@qq.com>
2024-02-04 17:31:21 +0800

    ArchInterrupt：更改实现方式

--- vk.kernel/arch/arm/cortex-m/inc/ArchInterrupt.h
+++ vk.kernel/arch/arm/cortex-m/inc/ArchInterrupt.h
@@ -7,25 +7,16 @@
 #ifndef __ARCH_INTERRUPT_H__
 #define __ARCH_INTERRUPT_H__
 
-#include "Templates.h"
+#include "Module.h"
 
 
 /// @brief Interrupt
-class ArchInterrupt
+class ArchInterrupt : public Module
 {
 public:
 	//Static constants
 	static const uint32_t isr_num = 256;
 	static const uint32_t rsvd_isr_size = 16;
-private:
-	//Members
-	uint32_t   isrSizes;
-	uint32_t*  vectors;
-public:
-	//Methods
-	ArchInterrupt();
-	~ArchInterrupt();
-	void Initialize();
 };
 
 #endif //!__INTERRUPT_H__
