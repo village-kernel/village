30c17e0684b549b9abd838f7f6b7a01b48493253
aicodeone <277579066@qq.com>
2024-02-05 22:53:49 +0800

    libcinit：更新文件

--- vk.hardware/arm/cortex-m/mcu/st/GCC/libcinit.c
+++ vk.hardware/arm/cortex-m/mcu/st/GCC/libcinit.c
@@ -1,25 +1,24 @@
-
-/// @brief execute preinit_arrary
-/// @param  
-void __libc_preinit_arrary(void)
-{
-	extern void (*__preinit_array_start []) (void);
-	extern void (*__preinit_array_end   []) (void);
-
-	int count = __preinit_array_end - __preinit_array_start;
-	for (int i = 0; i < count; i++)
-		__preinit_array_start[i]();
-}
-
+//###########################################################################
+// libcinit.c
+// libc initialize file
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
 
 /// @brief execute init_arrary
 /// @param  
 void __libc_init_array(void)
 {
+	extern void (*__preinit_array_start []) (void);
+	extern void (*__preinit_array_end   []) (void);
 	extern void (*__init_array_start []) (void);
 	extern void (*__init_array_end   []) (void);
 
-	int count = __init_array_end - __init_array_start;
+	int count = __preinit_array_end - __preinit_array_start;
+	for (int i = 0; i < count; i++)
+		__preinit_array_start[i]();
+
+	count = __init_array_end - __init_array_start;
 	for (int i = 0; i < count; i++)
 		__init_array_start[i]();
 }
