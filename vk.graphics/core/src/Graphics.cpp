a4b349ba847c080cde2582d0735aa05c839bea73
aicodeone <277579066@qq.com>
2024-02-06 19:57:45 +0800

    Driver：还原IOCtrl接口

--- vk.graphics/core/src/Graphics.cpp
+++ vk.graphics/core/src/Graphics.cpp
@@ -37,7 +37,7 @@ void Graphics::Initialize(const char* drvname)
 	//Get the specified lcd driver by driver ioctrl 
 	if (NULL != driver)
 	{
-		fbdev = (FBDriver*)driver->IOCtrl(0);
+		driver->IOCtrl(0, (void*)&fbdev);
 	}
 
 	//Initialize display
