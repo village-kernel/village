0eeb10b09192793c8a3ba1c4be67a368b125ff04
aicodeone <277579066@qq.com>
2024-04-24 00:03:59 +0800

    TextBox：粗劣增加显示逻辑

--- village-os/framework/vkgui/wedget/inc/TextBox.h
+++ village-os/framework/vkgui/wedget/inc/TextBox.h
@@ -8,11 +8,19 @@
 #define __VK_TEXT_BOX_H__
 
 #include "Wedget.h"
+#include "Mutex.h"
 
 ///TextBox
 class TextBox : public Wedget
 {
-
+private:
+	char* text;
+	Mutex lock;
+public:
+	void Initialize();
+	void Show();
+	void SetText(char* text);
+	void AppendText(char* text);
 };
 
 #endif //!__VK_TEXT_BOX_H__
--- village-os/framework/vkgui/wedget/src/TextBox.cpp
+++ village-os/framework/vkgui/wedget/src/TextBox.cpp
@@ -5,3 +5,93 @@
 // $Copyright: Copyright (C) village
 //###########################################################################
 #include "TextBox.h"
+
+
+/// @brief 
+void TextBox::Initialize()
+{
+	text = NULL;
+}
+
+
+/// @brief 
+void TextBox::Show()
+{
+	int locX = GetLocX();
+	int locY = GetLocY();
+	display->DrawRectangle(locX, locY, locX + width, locY + height);
+	if (text) display->ShowString(locX, locY, (uint8_t*)text);
+	Wedget::Show();
+}
+
+
+/// @brief 
+/// @param text 
+void TextBox::SetText(char* text)
+{
+	this->text = text;
+}
+
+
+/// @brief 
+/// @param text 
+void TextBox::AppendText(char* text)
+{
+	lock.Lock();
+
+	static Display::FontSize fontSize = Display::Font16;
+	static uint32_t xOffset = GetLocX() + 1;
+	static uint32_t yOffset = GetLocY() + 1;
+
+	uint32_t limitX = GetLocX() + width;
+	uint32_t limitY = GetLocY() + height;
+
+	while ('\0' != *text)
+	{
+		//New line
+		if ((xOffset + (fontSize >> 1) > limitX) || ('\n' == *text))
+		{
+			xOffset  = GetLocX() + 1;
+			yOffset += fontSize;
+		}
+
+		//Move up
+		if (yOffset + fontSize > limitY)
+		{
+			uint32_t y0 = (yOffset + fontSize) - limitY;
+			for (uint32_t y = GetLocY() + 1 + y0; y < limitY; y++)
+			{
+				for (uint32_t x = GetLocX() + 1; x < limitX; x++)
+				{
+					uint32_t color = display->ReadPoint(x, y);
+					display->DrawPoint(x, y - y0, color);
+				}
+			}
+
+			xOffset = GetLocX() + 1;
+			yOffset = limitY - fontSize;
+			
+			for (uint32_t y = yOffset; y < limitY; y++)
+			{
+				for (uint32_t x = GetLocX() + 1; x < limitX; x++)
+				{
+					display->DrawPoint(x, y, Display::White);
+				}
+			}
+		}
+
+		//Backspace one character 
+		if (*text == '\b') xOffset -= (fontSize >> 1);
+
+		//Normal display
+		if ((*text <= '~') && (*text >= ' '))
+		{
+			display->ShowChar(xOffset, yOffset, *text, fontSize, Display::NotMultiply, Display::Black);
+
+			xOffset += fontSize >> 1;
+		}
+
+		text++;
+	}
+	lock.Unlock();
+}
