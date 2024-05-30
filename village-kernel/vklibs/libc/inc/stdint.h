98469f4261990f3ef383bc977e991b57dfa2d8f4
aicodeone <277579066@qq.com>
2024-05-30 15:37:09 +0800

    stdint：使用gcc编译器的stdint头文件

--- village-kernel/vklibs/libc/inc/stdint.h
+++ village-kernel/vklibs/libc/inc/stdint.h
@@ -1,22 +1,12 @@
 //###########################################################################
 // stdint.h
-// Header file for stdint classes
+// Header file for stdint
 //
 // $Copyright: Copyright (C) village
 //###########################################################################
 #ifndef __STDINT_H__
 #define __STDINT_H__
 
-typedef signed char              int8_t;
-typedef signed short             int16_t;
-typedef long signed              int32_t;
-typedef long long signed int     int64_t;
-
-typedef unsigned char            uint8_t;
-typedef unsigned short           uint16_t;
-typedef long unsigned int        uint32_t;
-typedef long long unsigned int   uint64_t;
-
-//#define __ASM(code)     asm volatile(code)
+#include "stdint-gcc.h"
 
 #endif // !__STDINT_H__
