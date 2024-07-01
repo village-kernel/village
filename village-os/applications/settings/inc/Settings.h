e3b9252e2dc240dda1e64d19a22ebf0e26ae7982
aicodeone <277579066@qq.com>
2024-07-01 23:17:58 +0800

    Settings：初步增加模板

---
+++ village-os/applications/settings/inc/Settings.h
@@ -0,0 +1,22 @@
+//###########################################################################
+// Settings.h
+// The overall framework of the settings
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
+
+
+/// @brief Settings
+class Settings
+{
+public:
+	//Methods
+	Settings();
+	~Settings();
+	void Setup();
+	void Execute();
+};
+
+#endif //!__SETTINGS_H__
---
+++ village-os/applications/settings/src/Settings.cpp
@@ -0,0 +1,44 @@
+//###########################################################################
+// Settings.cpp
+// The overall framework of the Settings
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#include "Kernel.h"
+#include "Settings.h"
+
+
+/// @brief Constructor
+Settings::Settings()
+{
+}
+
+
+/// @brief Destructor
+Settings::~Settings()
+{
+}
+
+
+/// @brief Setup
+void Settings::Setup()
+{
+
+}
+
+
+/// @brief Execute
+void Settings::Execute()
+{
+	while (1) {}
+}
+
+
+/// @brief main
+int main(void)
+{
+	Settings settings;
+	settings.Setup();
+	settings.Execute();
+	return 0;
+}
