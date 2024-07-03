164ee4532fa58b99fffabd030798b38e8c002baf
aicodeone <277579066@qq.com>
2024-07-03 13:43:42 +0800

    Thread：修正pid为tid，thread创建的是线程不是进程

--- village-kernel/kernel/if/Kernel.h
+++ village-kernel/kernel/if/Kernel.h
@@ -117,7 +117,7 @@ public:
 	struct Task 
 	{
 		char*            name;
-		uint32_t         pid;
+		uint32_t         tid;
 		uint32_t         psp;
 		uint32_t         ticks;
 		uint32_t         stack;
@@ -125,7 +125,7 @@ public:
 		
 		Task(uint32_t stack = 0, char* name = NULL)
 			:name(name),
-			pid(-1),
+			tid(-1),
 			psp(0),
 			ticks(0),
 			stack(stack),
@@ -136,8 +136,8 @@ public:
 	///Methods
 	virtual int CreateTask(const char* name, Function function, void* user = NULL, void* args = NULL) = 0;
 	virtual int CreateTask(const char* name, Method method, Class *user, void* args = NULL) = 0;
-	virtual bool DeleteTask(int pid) = 0;
-	virtual bool WaitForTask(int pid) = 0;
+	virtual bool DeleteTask(int tid) = 0;
+	virtual bool WaitForTask(int tid) = 0;
 	virtual List<Task*> GetTasks() = 0;
 	virtual void Sleep(uint32_t ticks) = 0;
 	virtual void TaskExit() = 0;
