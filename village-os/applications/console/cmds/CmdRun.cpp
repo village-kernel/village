dc449a7263793ec9400c258c6e9fed4e81c67e2a
aicodeone <277579066@qq.com>
2024-07-04 23:17:20 +0800

    CmdRun：修改让命令后台运行逻辑

--- village-os/applications/console/cmds/CmdRun.cpp
+++ village-os/applications/console/cmds/CmdRun.cpp
@@ -19,21 +19,15 @@ public:
 	{
 		if (argc < 2)
 		{
-			console.Output("Usage: run [-b] <program> [arg1] [arg2] [...]");
+			console.Output("Usage: run <program> [arg1] [arg2] [...] [&]");
 			return;
 		}
 
 		Process::Behavior behavior = Process::_Foreground;
 
-		for (int i = 0; i < argc; i++)
+		if (0 == strcmp(argv[argc - 1], "&"))
 		{
-			if (0 == strcmp(argv[i], "-b"))
-			{
-				behavior = Process::_Background;
-				argc -= (i + 1);
-				argv += (i + 1);
-				break;
-			}
+			behavior = Process::_Background;
 		}
 
 		kernel->process.Run(behavior, argv[1], argc - 1, argv + 1);
