6b1081b9469b0678abd3fd0dc8c7b3180c60321b
aicodeone <277579066@qq.com>
2024-07-07 18:46:04 +0800

    crt0_app：entry section增加IMGOFFS和DYNAMIC值，把extern "C" 调整到function前面

--- village-os/framework/crt0/crt0_app.cpp
+++ village-os/framework/crt0/crt0_app.cpp
@@ -9,102 +9,106 @@
 /// @brief Kernel
 Kernel* kernel;
 
+/// @brief image offset
+extern uint32_t _IMGOFFS;
 
-/// @brief extern C
-extern "C" 
+/// @brief dynamic header
+extern uint32_t _DYNAMIC;
+
+
+/// @brief setup kernel
+/// @param kernel 
+/// @return 
+extern "C" void setup(void* knl)
 {
-	/// @brief setup kernel
-	/// @param kernel 
-	/// @return 
-	void setup(void* knl)
-	{
-		kernel = (Kernel*)knl;
-	}
-
-
-	/// @brief program entry main
-	/// @param argc 
-	/// @param argv 
-	/// @return 
-	int main(int argc, char* argv[]);
-
-
-	/// @brief fill bss zero
-	/// @param  
-	void __fill_bss_zero(void)
-	{
-		extern void *_sbss, *_ebss;
-
-		//Zero fill the bss segment.
-		for (void **pDest = &_sbss; pDest != &_ebss; pDest++)
-			*pDest = 0;
-	}
-
-
-	/// @brief execute preinit_arrary
-	/// @param  
-	void __preinit_arrary(void)
-	{
-		extern void (*__preinit_array_start []) (void);
-		extern void (*__preinit_array_end   []) (void);
-
-		int count = __preinit_array_end - __preinit_array_start;
-		for (int i = 0; i < count; i++)
-			__preinit_array_start[i]();
-	}
-
-
-	/// @brief execute init_arrary
-	/// @param  
-	void __init_array(void)
-	{
-		extern void (*__init_array_start []) (void);
-		extern void (*__init_array_end   []) (void);
-
-		int count = __init_array_end - __init_array_start;
-		for (int i = 0; i < count; i++)
-			__init_array_start[i]();
-	}
-
-
-	/// @brief execute fini_arrary
-	/// @param  
-	void __fini_array(void)
-	{
-		extern void (*__fini_array_start []) (void);
-		extern void (*__fini_array_end   []) (void);
-
-		int count = __fini_array_end - __fini_array_start;
-		
-		for (int i = 0; i < count; i++)
-		{
-			__fini_array_start[i]();
-		}
-	}
-
-
-	/// @brief _start
-	/// @param argc 
-	/// @param argv 
-	void _start(void* kernel, int argc, char* argv[])
-	{
-		__fill_bss_zero();
-
-		setup(kernel);
-
-		__preinit_arrary();
-
-		__init_array();
-
-		main(argc, argv);
-
-		__fini_array();
-	}
-
-
-	/// @brief entry_vector
-	void* g_pfnVectors[] __attribute__ ((section (".entry_vector"), used)) = 
-	{
-		(void*)&_start,
-	};
+	kernel = (Kernel*)knl;
 }
+
+
+/// @brief program entry main
+/// @param argc 
+/// @param argv 
+/// @return 
+extern "C" int main(int argc, char* argv[]);
+
+
+/// @brief fill bss zero
+/// @param  
+extern "C" void __fill_bss_zero(void)
+{
+	extern void *_sbss, *_ebss;
+
+	//Zero fill the bss segment.
+	for (void **pDest = &_sbss; pDest != &_ebss; pDest++)
+		*pDest = 0;
+}
+
+
+/// @brief execute preinit_arrary
+/// @param  
+extern "C" void __preinit_arrary(void)
+{
+	extern void (*__preinit_array_start []) (void);
+	extern void (*__preinit_array_end   []) (void);
+
+	int count = __preinit_array_end - __preinit_array_start;
+	for (int i = 0; i < count; i++)
+		__preinit_array_start[i]();
+}
+
+
+/// @brief execute init_arrary
+/// @param  
+extern "C" void __init_array(void)
+{
+	extern void (*__init_array_start []) (void);
+	extern void (*__init_array_end   []) (void);
+
+	int count = __init_array_end - __init_array_start;
+	for (int i = 0; i < count; i++)
+		__init_array_start[i]();
+}
+
+
+/// @brief execute fini_arrary
+/// @param  
+extern "C" void __fini_array(void)
+{
+	extern void (*__fini_array_start []) (void);
+	extern void (*__fini_array_end   []) (void);
+
+	int count = __fini_array_end - __fini_array_start;
+	
+	for (int i = 0; i < count; i++)
+	{
+		__fini_array_start[i]();
+	}
+}
+
+
+/// @brief _start
+/// @param argc 
+/// @param argv 
+extern "C" void _start(void* kernel, int argc, char* argv[])
+{
+	__fill_bss_zero();
+
+	setup(kernel);
+
+	__preinit_arrary();
+
+	__init_array();
+
+	main(argc, argv);
+
+	__fini_array();
+}
+
+
+/// @brief entry
+void* g_pfnVectors[] __attribute__ ((section (".entry"), used)) = 
+{	
+	(void*)&_IMGOFFS,
+	(void*)&_DYNAMIC,
+	(void*)&_start,
+};
