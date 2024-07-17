4d8f708ca444bc3284866704e501cc2787311c39
aicodeone <277579066@qq.com>
2024-07-17 16:28:50 +0800

    ArchInterrupt：优化Setup逻辑

--- village-kernel/arch/arm/cortex-m/src/ArchInterrupt.cpp
+++ village-kernel/arch/arm/cortex-m/src/ArchInterrupt.cpp
@@ -26,21 +26,19 @@ void ArchInterrupt::Setup()
 	for (; pDest != &_evector; pSource++, pDest++)
 		*pDest = *pSource;
 
-	//Calculate the size of isr vector, offset _estack and Reset_Handler
+	//Calculate the size of isr vector
 	uint32_t isrSizes = ((uint32_t)&_evector - (uint32_t)&_svector) >> 2;
-	isrSizes = isrSizes - 2;
 
-	//Calculate the address of usr isr vectors, offset _estack and Reset_Handler
-	uint32_t* vectors = (uint32_t*)&_svector + 2;
+	//Calculate the address of usr isr vectors
+	uint32_t* vectors = (uint32_t*)&_svector;
 
-	//Set the new isr handler
-	for (uint32_t i = 0; i < isrSizes; i++)
+	//Set the new isr handler, offset _estack and Reset_Handler
+	for (uint32_t i = 2; i < isrSizes; i++)
 	{
-		//Change the origin handler into isr table, offset _estack and Reset_Handler
+		//Change the origin handler into isr table
 		if (vectors[i] != (uint32_t)&(Default_Handler) + 1)
 		{
-			int irq = i - (rsvd_isr_size - 2);
-			kernel->interrupt.SetISR(irq, (Function)vectors[i], NULL, NULL);
+			kernel->interrupt.SetISR(i - rsvd_isr_size, (Function)vectors[i], NULL, NULL);
 		}
 
 		vectors[i] = (uint32_t)&Stub_Handler;
