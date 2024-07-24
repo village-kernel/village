94b86090855291e86f1a4c89c95233af4274e517
aicodeone <277579066@qq.com>
2024-07-24 13:22:25 +0800

    Exception：重新编写

--- village-kernel/arch/arm/cortex-m/src/Exception.cpp
+++ village-kernel/arch/arm/cortex-m/src/Exception.cpp
@@ -6,12 +6,11 @@
 //###########################################################################
 #include "Exception.h"
 #include "Hardware.h"
+#include "Registers.h"
 
 
 /// @brief Constructor
 Exception::Exception()
-	:debug(NULL),
-	interrupt(NULL)
 {
 }
 
@@ -25,77 +24,97 @@ Exception::~Exception()
 /// @brief Exception Setup
 void Exception::Setup()
 {
-	//Gets the debug pointer
-	debug = (Debug*)&kernel->debug;
-
-	//Gets the interrupt pointer
-	interrupt = (Interrupt*)&kernel->interrupt;
-
-	//Set interrupt service
-	interrupt->AppendISR(NonMaskableInt_IRQn,              (Method)&Exception::NMIHandler,            this);
-	interrupt->AppendISR(HardFault_IRQn,                   (Method)&Exception::HardFaultHandler,      this);
-	interrupt->AppendISR(MemoryManagement_IRQn,            (Method)&Exception::MemManageHandler,      this);
-	interrupt->AppendISR(BusFault_IRQn,                    (Method)&Exception::BusFaultHandler,       this);
-	interrupt->AppendISR(UsageFault_IRQn,                  (Method)&Exception::UsageFaultHandler,     this);
-	interrupt->AppendISR(DebugMonitor_IRQn,                (Method)&Exception::DebugMonHandler,       this);
+	kernel->interrupt.Replace(NonMaskableInt_IRQn,     (uint32_t)&Exception::NMIHandler         );
+	kernel->interrupt.Replace(HardFault_IRQn,          (uint32_t)&Exception::HardFaultHandler   );
+	kernel->interrupt.Replace(MemoryManagement_IRQn,   (uint32_t)&Exception::MemManageHandler   );
+	kernel->interrupt.Replace(BusFault_IRQn,           (uint32_t)&Exception::BusFaultHandler    );
+	kernel->interrupt.Replace(UsageFault_IRQn,         (uint32_t)&Exception::UsageFaultHandler  );
+	kernel->interrupt.Replace(DebugMonitor_IRQn,       (uint32_t)&Exception::DebugMonHandler    );
 }
 
 
 /// @brief Exception Exit
 void Exception::Exit()
 {
-	interrupt->RemoveISR(NonMaskableInt_IRQn,             (Method)&Exception::NMIHandler,            this);
-	interrupt->RemoveISR(HardFault_IRQn,                  (Method)&Exception::HardFaultHandler,      this);
-	interrupt->RemoveISR(MemoryManagement_IRQn,           (Method)&Exception::MemManageHandler,      this);
-	interrupt->RemoveISR(BusFault_IRQn,                   (Method)&Exception::BusFaultHandler,       this);
-	interrupt->RemoveISR(UsageFault_IRQn,                 (Method)&Exception::UsageFaultHandler,     this);
-	interrupt->RemoveISR(DebugMonitor_IRQn,               (Method)&Exception::DebugMonHandler,       this);
 }
 
 
 /// @brief NMIHandler
-void Exception::NMIHandler()
+void __attribute__ ((naked)) Exception::NMIHandler()
 {
-	debug->Error("NMI Handler");
-	while(1) {}
+	ExceptionInfo("NMI Handler");
 }
 
 
 /// @brief HardFaultHandler
-void Exception::HardFaultHandler()
+void __attribute__ ((naked)) Exception::HardFaultHandler()
 {
-	debug->Error("Hard Fault Handler");
-	while(1) {}
+	ExceptionInfo("Hard Fault Handler");
 }
 
 
 /// @brief MemManageHandler
-void Exception::MemManageHandler()
+void __attribute__ ((naked)) Exception::MemManageHandler()
 {
-	debug->Error("Mem Manage Handler");
-	while(1) {}
+	ExceptionInfo("Mem Manage Handler");
 }
 
 
 /// @brief BusFaultHandler
-void Exception::BusFaultHandler()
+void __attribute__ ((naked)) Exception::BusFaultHandler()
 {
-	debug->Error("Bus Fault Handler");
-	while(1) {}
+	ExceptionInfo("Bus Fault Handler");
 }
 
 
 /// @brief UsageFaultHandler
-void Exception::UsageFaultHandler()
+void __attribute__ ((naked)) Exception::UsageFaultHandler()
 {
-	debug->Error("Usage Fault Handler");
-	while(1) {}
+	ExceptionInfo("Usage Fault Handler");
 }
 
 
 /// @brief DebugMonHandler
-void Exception::DebugMonHandler()
+void __attribute__ ((naked)) Exception::DebugMonHandler()
 {
-	debug->Error("Debug Mon Handler");
+	ExceptionInfo("Debug Mon Handler");
+}
+
+
+/// @brief Exception Info
+/// @param exception 
+inline void Exception::ExceptionInfo(const char* exception)
+{
+	__asm volatile("tst lr, #4");
+	__asm volatile("ite eq");
+	__asm volatile("mrseq r0, msp");
+	__asm volatile("mrsne r0, psp");
+	__asm volatile("stmdb r0!, {r4-r11}");
+	__asm volatile("mov r1, %0" : "+r"(exception));
+	__asm volatile("b StackedInfo");
+}
+
+
+/// @brief Output stacked info
+/// @param regs stack pointer
+extern "C" void StackedInfo(Registers* regs, char* exception)
+{
+	kernel->debug.Error("r0:   0x%08lx", regs->r0);
+	kernel->debug.Error("r1:   0x%08lx", regs->r1);
+	kernel->debug.Error("r2:   0x%08lx", regs->r2);
+	kernel->debug.Error("r3:   0x%08lx", regs->r3);
+	kernel->debug.Error("r4:   0x%08lx", regs->r4);
+	kernel->debug.Error("r5:   0x%08lx", regs->r5);
+	kernel->debug.Error("r6:   0x%08lx", regs->r6);
+	kernel->debug.Error("r7:   0x%08lx", regs->r7);
+	kernel->debug.Error("r8:   0x%08lx", regs->r8);
+	kernel->debug.Error("r9:   0x%08lx", regs->r9);
+	kernel->debug.Error("r10:  0x%08lx", regs->r10);
+	kernel->debug.Error("r11:  0x%08lx", regs->r11);
+	kernel->debug.Error("r12:  0x%08lx", regs->r12);
+	kernel->debug.Error("lr:   0x%08lx", regs->lr);
+	kernel->debug.Error("pc:   0x%08lx", regs->pc);
+	kernel->debug.Error("xpsr: 0x%08lx", regs->xpsr);
+	kernel->debug.Error(exception);
 	while(1) {}
 }
