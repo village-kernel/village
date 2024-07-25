8ed17c034f4c410a2f2176681b0852dbb1f598a7
aicodeone <277579066@qq.com>
2024-07-26 00:58:18 +0800

    SDMMC：完善sdmmc卡驱动

--- village-kernel/drivers/arm/cortex-m/src/SDMMC.cpp
+++ village-kernel/drivers/arm/cortex-m/src/SDMMC.cpp
@@ -13,7 +13,27 @@
 /// @brief SDMMC
 class SDMMC : public Driver
 {
+public:
+	//Enumerations
+	enum IOCtrlCmd
+	{
+		_CtrlSync       = 0,
+		_GetSectorCount = 1,
+		_GetSectorSize  = 2,
+		_GetBlockSzie   = 3,
+		_Ctrltrim       = 4,
+
+		_GetType        = 10,
+		_GetCSDData     = 11,
+		_GetCIDData     = 12,
+		_GetOCRData     = 13,
+		_GetSdstat      = 14,
+	};
 private:
+	//const static members
+	const static uint32_t sd_timeout = 30000;
+	const static uint32_t sd_block_size = 512;
+
 	//Members
 	Sdio sdio;
 	Sdio::Config config;
@@ -45,6 +65,41 @@ private:
 		config.cmdPin    = SDMMC_CMD_PIN;
 		config.cmdAltNum = SDMMC_CMD_AF_NUM;
 	}
+private:
+	/// @brief GetTicks
+	/// @return 
+	uint32_t GetTicks()
+	{
+		return kernel->system.GetSysClkCounts();
+	}
+
+
+	/// @brief CheckStatus
+	/// @return 
+	bool CheckStatus()
+	{
+		return (sdio.GetCardState() == Sdio::MSD_OK);
+	}
+
+
+	/// @brief CheckStatusWithTimeout
+	/// @param timeout 
+	/// @return 
+	bool CheckStatusWithTimeout(uint32_t timeout)
+	{
+		uint32_t timer = GetTicks();
+
+		//block until SDIO IP is ready again or a timeout occur
+		while (GetTicks() - timer < timeout)
+		{
+			if (sdio.GetCardState() == Sdio::SD_TRANSFER_OK)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 public: 
 	/// @brief Constructor
 	SDMMC()
@@ -65,9 +120,12 @@ public:
 		InitConfig();
 
 		//Initialize sdio
-		sdio.Initialize(config);
+		if (Sdio::MSD_OK == sdio.Initialize(config))
+		{
+			return CheckStatus();
+		}
 
-		return true;
+		return false;
 	}
 
 
@@ -80,23 +138,136 @@ public:
 
 	/// @brief Write data
 	/// @param data 
-	/// @param count 
-	/// @param blk 
+	/// @param blkSize 
+	/// @param sector 
 	/// @return 
-	int Write(uint8_t* data, uint32_t count = 0, uint32_t blk = 0)
+	int Write(uint8_t* data, uint32_t blkSize, uint32_t sector)
 	{
-		return sdio.Write(data, count, blk);
+		uint32_t count = 0;
+		uint32_t timeout = 0;
+
+		sdio.ClearWriteStatus();
+
+		if (CheckStatusWithTimeout(sd_timeout) == false)
+		{
+			return count;
+		}
+
+		if (sdio.WriteBlocksDMA(data, blkSize, sector) == Sdio::MSD_OK)
+		{
+			//Wait that writing process is completed or a timeout occurs
+			timeout = GetTicks();
+			while ((sdio.GetWriteStatus() == false) && ((GetTicks() - timeout) < sd_timeout)) {}
+
+			//in case of a timeout return error
+			if (sdio.GetWriteStatus() == false)
+			{
+				count = 0;
+			}
+			else
+			{
+				sdio.ClearWriteStatus();
+				timeout = GetTicks();
+
+				while ((GetTicks() - timeout) < sd_timeout)
+				{
+					if (sdio.GetCardState() == Sdio::SD_TRANSFER_OK)
+					{
+						count = blkSize;
+						break;
+					}
+				}
+			}
+		}
+
+		return count;
 	}
 
 
 	/// @brief Read data
 	/// @param data 
-	/// @param count 
-	/// @param blk 
+	/// @param blkSize 
+	/// @param sector 
 	/// @return 
-	int Read(uint8_t* data, uint32_t count = 0, uint32_t blk = 0)
+	int Read(uint8_t* data, uint32_t blkSize, uint32_t sector)
 	{
-		return sdio.Read(data, count, blk);
+		uint32_t count = 0;
+		uint32_t timeout = 0;
+
+		//ensure the SDCard is ready for a new operation
+		if (CheckStatusWithTimeout(sd_timeout) == false)
+		{
+			return count;
+		}
+
+		if (sdio.ReadBlocksDMA(data, blkSize, sector) == Sdio::MSD_OK)
+		{
+			sdio.ClearReadStatus();
+			
+			//Wait that the reading process is completed or a timeout occurs
+			timeout = GetTicks();
+			while((sdio.GetReadStatus() == false) && ((GetTicks() - timeout) < sd_timeout)) {}
+			
+			//in case of a timeout return error
+			if (sdio.GetReadStatus() == false)
+			{
+				count = 0;
+			}
+			else
+			{
+				sdio.ClearReadStatus();
+				timeout = GetTicks();
+
+				while ((GetTicks() - timeout) < sd_timeout)
+				{
+					if (sdio.GetCardState() == Sdio::SD_TRANSFER_OK)
+					{
+						count = blkSize;
+						break;
+					}
+				}
+			}
+		}
+
+		return count;
+	}
+
+
+	/// @brief SDMMC io ctrl
+	/// @param cmd 
+	/// @param data 
+	/// @return 
+	int IOCtrl(uint8_t cmd, void *data)
+	{
+		Sdio::SDCardInfo cardInfo;
+
+		switch (cmd)
+		{
+			//Make sure that no pending write process
+			case _CtrlSync :
+				break;
+
+			//Get number of sectors on the disk (uint32_t)
+			case _GetSectorCount :
+				sdio.GetCardInfo(&cardInfo);
+				*(uint32_t*)data = cardInfo.LogBlockNbr;
+				break;
+
+			//Get R/W sector size (uint32_t)
+			case _GetSectorSize :
+				sdio.GetCardInfo(&cardInfo);
+				*(uint32_t*)data = cardInfo.LogBlockSize;
+				break;
+
+			//Get erase block size in unit of sector (uint32_t)
+			case _GetBlockSzie :
+				sdio.GetCardInfo(&cardInfo);
+				*(uint32_t*)data = cardInfo.LogBlockSize / sd_block_size;
+				break;
+
+			default: break;
+		}
+		return 0;
 	}
 };
 
