fdafff53ea3ad4e06c6ad51298a4cb0cf1607b26
aicodeone <277579066@qq.com>
2024-08-11 02:08:28 +0800

    xxxEexecutor：修正Initiate错误返回值

--- village-kernel/binutils/src/ElfExecutor.cpp
+++ village-kernel/binutils/src/ElfExecutor.cpp
@@ -13,7 +13,7 @@
 int ElfExecutor::Initiate()
 {
 	//Load, parser and execute elf file
-	if (!elf.Load(path)) return 0;
+	if (!elf.Load(path)) return -1;
 
 	//Create a sandboxed thread to run the app
 	return kernel->thread.CreateTask(path, (Method)&ElfExecutor::Sandbox, this);
