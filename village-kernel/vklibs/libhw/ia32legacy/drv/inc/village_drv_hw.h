9350f4a45b7e539c25bf1fd2f9d82bc356d1c974
aicodeone <277579066@qq.com>
2024-08-12 01:02:47 +0800

    libhw/ia32legacy：增加village_drv_hw.h

---
+++ village-kernel/vklibs/libhw/ia32legacy/drv/inc/village_drv_hw.h
@@ -0,0 +1,14 @@
+//###########################################################################
+// village_drv_hw.h
+// Header file for HAL classes
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __VILLAGE_DRV_HW_H__
+#define __VILLAGE_DRV_HW_H__
+
+#include "i386.h"
+#include "PCIController.h"
+#include "PS2Controller.h"
+
+#endif //!__VILLAGE_DRV_HW_H__
