46715a59d11296332539291cfc047fc2b8a9104b
aicodeone <277579066@qq.com>
2024-08-15 23:55:22 +0800

    drv_gpio：优化细节

--- village-kernel/drivers/include/stm32h743_config.h
+++ village-kernel/drivers/include/stm32h743_config.h
@@ -17,12 +17,12 @@
 	.enableRTS = false,                                                   \
 	.enableDMA = true,                                                    \
                                                                           \
-	.txGpio = { Gpio::_ChA, 9,  Gpio::_Alt, 7, 0 },                       \
-	.rxGpio = { Gpio::_ChA, 10, Gpio::_Alt, 7, 0 },                       \
+	.txGpio = { Gpio::_ChA, 9,  Gpio::_Altera, 7, 0 },                    \
+	.rxGpio = { Gpio::_ChA, 10, Gpio::_Altera, 7, 0 },                    \
 	.deGpio = { 0, 0, 0, 0, 0 },                                          \
                                                                           \
-	.txDma = { Dma::_Group1, Dma::_Stream3, 42, NULL, false },            \
-	.rxDma = { Dma::_Group1, Dma::_Stream2, 41, NULL, true  },            \
+	.txDma = { Dma::_Group1, Dma::_Stream3, 42, NULL, Dma::_MemoryToPeriph },\
+	.rxDma = { Dma::_Group1, Dma::_Stream2, 41, NULL, Dma::_PeriphToMemory },\
 }
 
 
@@ -31,9 +31,9 @@
 {                                                                         \
 	.spiCh    = Spi::_Spi5,                                               \
                                                                           \
-	.sckGpio  = { Gpio::_ChF, 7,  Gpio::_Alt,    5, 0 },                  \
-	.misoGpio = { Gpio::_ChF, 8,  Gpio::_Alt,    5, 0 },                  \
-	.mosiGpio = { Gpio::_ChF, 9,  Gpio::_Alt,    5, 0 },                  \
+	.sckGpio  = { Gpio::_ChF, 7,  Gpio::_Altera, 5, 0 },                  \
+	.misoGpio = { Gpio::_ChF, 8,  Gpio::_Altera, 5, 0 },                  \
+	.mosiGpio = { Gpio::_ChF, 9,  Gpio::_Altera, 5, 0 },                  \
 	.csGpio   = { Gpio::_ChF, 6,  Gpio::_Output, 0, 0 },                  \
 	.wpGpio   = { 0, 0, 0, 0, 0 },                                        \
 }
@@ -42,12 +42,12 @@
 //********************************SDMMC***********************************//
 #define SDMMC_CONFIG                                                      \
 {                                                                         \
-	.d0Gpio  = { Gpio::_ChC, 8,  Gpio::_Alt,  12, 0 },                    \
-	.d1Gpio  = { Gpio::_ChC, 9,  Gpio::_Alt,  12, 0 },                    \
-	.d2Gpio  = { Gpio::_ChC, 10, Gpio::_Alt,  12, 0 },                    \
-	.d3Gpio  = { Gpio::_ChC, 11, Gpio::_Alt,  12, 0 },                    \
-	.ckGpio  = { Gpio::_ChC, 12, Gpio::_Alt,  12, 0 },                    \
-	.cmdGpio = { Gpio::_ChD, 2,  Gpio::_Alt,  12, 0 },                    \
+	.d0Gpio  = { Gpio::_ChC, 8,  Gpio::_Altera, 12, 0 },                  \
+	.d1Gpio  = { Gpio::_ChC, 9,  Gpio::_Altera, 12, 0 },                  \
+	.d2Gpio  = { Gpio::_ChC, 10, Gpio::_Altera, 12, 0 },                  \
+	.d3Gpio  = { Gpio::_ChC, 11, Gpio::_Altera, 12, 0 },                  \
+	.ckGpio  = { Gpio::_ChC, 12, Gpio::_Altera, 12, 0 },                  \
+	.cmdGpio = { Gpio::_ChD, 2,  Gpio::_Altera, 12, 0 },                  \
 }
 
 
