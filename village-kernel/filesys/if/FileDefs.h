ae6bdba7d0fb611551319127599f7ba5a6b12453
aicodeone <277579066@qq.com>
2024-08-28 00:12:46 +0800

    filesys：更新接口，整理代码

--- village-kernel/filesys/if/FileDefs.h
+++ village-kernel/filesys/if/FileDefs.h
@@ -66,21 +66,6 @@ struct FileDir
 };
 
 
-/// @brief MountNode
-struct MountNode
-{
-	char*    target;
-	char*    source;
-	uint16_t access;
-
-	MountNode(char* target, char* source, uint16_t access)
-		:target(target),
-		source(source),
-		access(access)
-	{}
-};
-
-
 ///Filesystem register macro
 #define REGISTER_FS(fs, name)                                 \
 static struct _FS_##name {                                    \
--- village-kernel/filesys/inc/FileSystem.h
+++ village-kernel/filesys/inc/FileSystem.h
@@ -16,15 +16,23 @@
 /// @brief ConcreteFileSystem
 class ConcreteFileSystem : public FileSystem
 {
-	private:
-	/// @brief Enumerates
+private:
+	/// @brief BootIndicator
 	enum BootIndicator
 	{
 		_NotBootable = 0x00,
 		_Bootable    = 0x80,
 	};
 
-	/// @brief Structures
+	/// @brief PartitionTableType
+	enum PartitionType
+	{
+		_None = 0,
+		_MBR,
+		_GPT,
+	};
+
+	/// @brief DPT
 	struct DPT
 	{
 		uint32_t bootIndicator : 8;
@@ -39,20 +47,49 @@ class ConcreteFileSystem : public FileSystem
 		uint32_t totalSectors;
 	} __attribute__((packed));
 
+	/// @brief MBR
 	struct MBR
 	{
 		uint8_t  boot[446];
 		DPT      dpt[4];
 		uint16_t magic;
 	} __attribute__((packed));
+
+	/// @brief MountNode
+	struct MountNode
+	{
+		char*    target;
+		char*    source;
+		uint16_t access;
+
+		MountNode(char* target, char* source, uint16_t access)
+			:target(target),
+			source(source),
+			access(access)
+		{}
+	};
+
+	/// @brief DiskMedia
+	struct DiskMedia
+	{
+		int            type;
+		char*          name;
+		List<FileVol*> vols;
+
+		DiskMedia(int type, char* name)
+			:type(type),
+			name(name)
+		{}
+	};
 private:
 	/// @brief Members
 	List<FileSys*>    fileSys;
-	List<FileVol*>    volumes;
+	List<DiskMedia*>  medias;
 	List<MountNode*>  mounts;
 
 	/// @brief Methods
-	bool InitMBRDisk(const char* disk);
+	int  AttachVolume(DiskMedia* media, FileVol* volume);
+	int  DetachVolume(DiskMedia* media, FileVol* volume);
 	bool MountSystemNode();
 public:
 	/// @brief Methods
@@ -60,14 +97,16 @@ public:
 	~ConcreteFileSystem();
 	void Setup();
 	void Exit();
-	
+
 	/// @brief Register Methods
 	void RegisterFS(FileSys* fs, const char* name);
 	void UnregisterFS(FileSys* fs, const char* name);
+
+	/// @brief Hard Drive Methods
+	bool MountHardDrive(const char* disk);
+	bool UnmountHardDrive(const char* disk);
 	
 	/// @brief Volume Methods
-	int AttachVolume(FileVol* volume);
-	int DetachVolume(FileVol* volume);
 	FileVol* GetVolume(const char* name);
 };
 
