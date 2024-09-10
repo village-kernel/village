7f9d9c422b6c968af0518360a71b16512687495d
aicodeone <277579066@qq.com>
2024-09-10 23:40:58 +0800

    CmdMsgMgr：拆分Receiving，优化代码

--- village-kernel/terminal/src/CmdMsgMgr.cpp
+++ village-kernel/terminal/src/CmdMsgMgr.cpp
@@ -12,10 +12,11 @@
 
 /// @brief Constructor
 CmdMsgMgr::CmdMsgMgr()
-	:txBufPos(0),
+	:hasMessage(false),
+	inputMode(_EDIT),
+	txBufPos(0),
 	rxBufPos(0),
-	history(0),
-	inputMode(_EDIT)
+	history(0)
 {
 	rxMsg.cmd = cmdBuffer;
 }
@@ -177,248 +178,284 @@ bool CmdMsgMgr::Receiving()
 
 			switch (inputMode)
 			{
-				case _INSERT:
-				{
-					//ASCII 32(space) ~ 126(~)
-					if (byte >= 0x20 && byte <= 0x7e)
-					{
-						//Move char[rxBufPos] to char[rxBufPos+1] and insert new char in char[rxBufPos]
-						for (uint16_t i = strlen((const char*)rxBuffer); i > rxBufPos ; i--)
-						{
-							rxBuffer[i] = rxBuffer[i-1];
-						}
-						rxBuffer[rxBufPos++] = byte;
-						
-						//Sent new string
-						uint16_t back = 0;
-						txBuffer[txBufPos++] = byte;
-						for (uint16_t i = rxBufPos; '\0' != rxBuffer[i]; i++)
-						{
-							txBuffer[txBufPos++] = rxBuffer[i];
-							back++;
-						}
+				case _INSERT: if (InsertMode(byte)) continue;
+				case _EDIT:       EditMode(byte);   break;
+				case _ANSI:       ANSIMode(byte);   break;
+				case _DCS:        DCSMode(byte);    break;
+				case _CSI:        CSIMode(byte);    break;
+				case _OSC:        OSCMode(byte);    break;
+				default:                            break;
+			}
 
-						//Move cursor back
-						for (uint16_t i = 0; i < back; i++)
-						{
-							txBuffer[txBufPos++] = '\b';
-						}
-
-						continue;
-					}
-
-					//ASCII DEL
-					else if (0x7f == byte || '\b' == byte)
-					{
-						if (rxBufPos)
-						{
-							//Del char[rxBufPos] and move char[rxBufPos] to char[rxBufPos-1]
-							rxBufPos--;
-							for (uint16_t i = rxBufPos; '\0' != rxBuffer[i]; i++)
-							{
-								rxBuffer[i] = rxBuffer[i+1];
-							}
-
-							//Sent new string
-							uint16_t back = 0;
-							txBuffer[txBufPos++] = '\b';
-							for (uint16_t i = rxBufPos; '\0' != rxBuffer[i]; i++)
-							{
-								txBuffer[txBufPos++] = rxBuffer[i];
-								back++;
-							}
-							txBuffer[txBufPos++] = ' ';
-							back++;
-
-							//Move cursor back
-							for (uint16_t i = 0; i < back; i++)
-							{
-								txBuffer[txBufPos++] = '\b';
-							}
-						}
-						continue;
-					}
-
-					inputMode = _EDIT;
-				}
-
-				case _EDIT:
-				{
-					//ANSI ESC
-					if ('\033' == byte)
-					{
-						inputMode = _ANSI;
-					}
-
-					//ASCII 32(space) ~ 126(~)
-					else if (byte >= 0x20 && byte <= 0x7e)
-					{
-						txBuffer[txBufPos++] = byte;
-						rxBuffer[rxBufPos++] = byte;
-						rxBuffer[rxBufPos+1] = '\0';
-					}
-
-					//ASCII DEL
-					else if (0x7f == byte || '\b' == byte)
-					{
-						if (rxBufPos)
-						{
-							//Backspace one character
-							rxBuffer[--rxBufPos] = '\0';
-
-							//Backspace character on terminal
-							txBuffer[txBufPos++] = '\b';
-							txBuffer[txBufPos++] = ' ';
-							txBuffer[txBufPos++] = '\b';
-						}
-					}
-
-					//ASCII CR
-					else if (0x0d == byte)
-					{
-						//Check is null cmd 
-						if (rxBufPos <= 0)
-						{
-							strcpy((char*)cmdBuffer, "null");
-							return true;
-						}
-
-						//Set command
-						for (uint16_t i = 0; ; i++)
-						{
-							if ((' '  == rxBuffer[i]) ||
-								('\r' == rxBuffer[i]) ||
-								('\0' == rxBuffer[i]))
-							{
-								cmdBuffer[i] = '\0'; break;
-							}
-
-							cmdBuffer[i] = rxBuffer[i];
-						}
-
-						//Record history
-						RecordHistory();
-
-						//Set args
-						rxMsg.args = cmdHistory[cmd_history_size - 2];
-
-						//Reset rxBufPos
-						rxBufPos = 0;
-						rxBuffer[rxBufPos] = '\0';
-						return true;
-					}
-				}
-				break;
-
-				case _ANSI:
-				{
-					switch (byte)
-					{
-						case 'N': //ESC N | 0x8e | SS2 – Single Shift Two
-							break;
-						case 'O': //ESC O | 0x8f | SS3 – Single Shift Three
-							break;
-						case 'P': //ESC P | 0x90 | DCS – Device Control String
-							inputMode = _DCS;
-							break;
-						case '[': //ESC [ | 0x9b | CSI - Control Sequence Introducer
-							inputMode = _CSI;
-							break;
-						case '\\'://ESC \ | 0x9c | ST – String Terminator
-							break;
-						case ']': //ESC ] | 0x9d | OSC – Operating System Command
-							inputMode = _OSC;
-							break;
-						case 'X': //ESC X | 0x98 | SOS – Start of String
-							break;
-						case '^': //ESC ^ | 0x9e | PM – Privacy Message
-							break;
-						case '_': //ESC _ | 0x9f | APC – Application Program Command
-							break;
-						case 'c': //ESC c |      | RIS – Reset to Initial State
-							break;
-						default: break;
-					}
-				}
-				break;
-
-				case _DCS:
-				{
-					
-				}
-				break;
-
-				case _CSI:
-				{
-					//Param byte
-					if (byte >= 0x30 && byte <= 0x3f)
-					{
-
-					}
-
-					//Middle byte
-					if (byte >= 0x20 && byte <= 0x2f)
-					{
-
-					}
-
-					//Final byte
-					if (byte >= 0x40 && byte <= 0x7e)
-					{
-						switch (byte)
-						{
-							case 'A': //up
-								if (NULL != cmdHistory[history-1])
-								{
-									RecordTempCmd();
-									history--;
-									RestoredHistory();
-								}
-								break;
-							case 'B': //down
-								if (history < cmd_history_size - 1)
-								{
-									history++;
-									RestoredHistory();
-								}
-								break;
-							case 'C': //right
-								if ('\0' != rxBuffer[rxBufPos])
-								{
-									txBuffer[txBufPos++] = rxBuffer[rxBufPos++];
-									inputMode = _INSERT;
-									continue;
-								}
-								break;
-							case 'D': //left
-								if (rxBufPos)
-								{
-									rxBufPos--;
-									txBuffer[txBufPos++] = '\b';
-									inputMode = _INSERT;
-									continue;
-								}
-								break;
-							case 'm': //SGR
-								break;
-							default: break;
-						}
-
-						inputMode = _EDIT;
-					}
-				}
-				break;
-
-				case _OSC:
-				{
-					if ('\0' == byte) inputMode = _EDIT;
-				}
-				break;
-
-				default: break;
+			if (true == hasMessage)
+			{
+				hasMessage = false;
+				return true;
 			}
 		}
 	}
 
 	return false;
+}
+
+
+/// @brief Insert Mode
+/// @param byte 
+/// @return 
+bool CmdMsgMgr::InsertMode(uint8_t byte)
+{
+	//ASCII 32(space) ~ 126(~)
+	if (byte >= 0x20 && byte <= 0x7e)
+	{
+		//Move char[rxBufPos] to char[rxBufPos+1] and insert new char in char[rxBufPos]
+		for (uint16_t i = strlen((const char*)rxBuffer); i > rxBufPos ; i--)
+		{
+			rxBuffer[i] = rxBuffer[i-1];
+		}
+		rxBuffer[rxBufPos++] = byte;
+		
+		//Sent new string
+		uint16_t back = 0;
+		txBuffer[txBufPos++] = byte;
+		for (uint16_t i = rxBufPos; '\0' != rxBuffer[i]; i++)
+		{
+			txBuffer[txBufPos++] = rxBuffer[i];
+			back++;
+		}
+
+		//Move cursor back
+		for (uint16_t i = 0; i < back; i++)
+		{
+			txBuffer[txBufPos++] = '\b';
+		}
+
+		return true;
+	}
+
+	//ASCII DEL
+	else if (0x7f == byte || '\b' == byte)
+	{
+		if (rxBufPos)
+		{
+			//Del char[rxBufPos] and move char[rxBufPos] to char[rxBufPos-1]
+			rxBufPos--;
+			for (uint16_t i = rxBufPos; '\0' != rxBuffer[i]; i++)
+			{
+				rxBuffer[i] = rxBuffer[i+1];
+			}
+
+			//Sent new string
+			uint16_t back = 0;
+			txBuffer[txBufPos++] = '\b';
+			for (uint16_t i = rxBufPos; '\0' != rxBuffer[i]; i++)
+			{
+				txBuffer[txBufPos++] = rxBuffer[i];
+				back++;
+			}
+			txBuffer[txBufPos++] = ' ';
+			back++;
+
+			//Move cursor back
+			for (uint16_t i = 0; i < back; i++)
+			{
+				txBuffer[txBufPos++] = '\b';
+			}
+		}
+
+		return true;
+	}
+
+	inputMode = _EDIT;
+
+	return false;
+}
+
+
+/// @brief Edit Mode
+/// @param byte 
+/// @return 
+void CmdMsgMgr::EditMode(uint8_t byte)
+{
+	//ANSI ESC
+	if ('\033' == byte)
+	{
+		inputMode = _ANSI;
+	}
+
+	//ASCII 32(space) ~ 126(~)
+	else if (byte >= 0x20 && byte <= 0x7e)
+	{
+		txBuffer[txBufPos++] = byte;
+		rxBuffer[rxBufPos++] = byte;
+		rxBuffer[rxBufPos+1] = '\0';
+	}
+
+	//ASCII DEL
+	else if (0x7f == byte || '\b' == byte)
+	{
+		if (rxBufPos)
+		{
+			//Backspace one character
+			rxBuffer[--rxBufPos] = '\0';
+
+			//Backspace character on terminal
+			txBuffer[txBufPos++] = '\b';
+			txBuffer[txBufPos++] = ' ';
+			txBuffer[txBufPos++] = '\b';
+		}
+	}
+
+	//ASCII CR
+	else if (0x0d == byte)
+	{
+		//Check is null cmd 
+		if (rxBufPos <= 0)
+		{
+			strcpy((char*)cmdBuffer, "null");
+			hasMessage = true;
+			return;
+		}
+
+		//Set command
+		for (uint16_t i = 0; ; i++)
+		{
+			if ((' '  == rxBuffer[i]) ||
+				('\r' == rxBuffer[i]) ||
+				('\0' == rxBuffer[i]))
+			{
+				cmdBuffer[i] = '\0'; break;
+			}
+
+			cmdBuffer[i] = rxBuffer[i];
+		}
+
+		//Record history
+		RecordHistory();
+
+		//Set args
+		rxMsg.args = cmdHistory[cmd_history_size - 2];
+
+		//Reset rxBufPos
+		rxBufPos = 0;
+		rxBuffer[rxBufPos] = '\0';
+		hasMessage = true;
+		return;
+	}
+}
+
+
+/// @brief ANSI Mode
+/// @param byte 
+/// @return 
+void CmdMsgMgr::ANSIMode(uint8_t byte)
+{
+	switch (byte)
+	{
+		case 'N': //ESC N | 0x8e | SS2 – Single Shift Two
+			break;
+		case 'O': //ESC O | 0x8f | SS3 – Single Shift Three
+			break;
+		case 'P': //ESC P | 0x90 | DCS – Device Control String
+			inputMode = _DCS;
+			break;
+		case '[': //ESC [ | 0x9b | CSI - Control Sequence Introducer
+			inputMode = _CSI;
+			break;
+		case '\\'://ESC \ | 0x9c | ST – String Terminator
+			break;
+		case ']': //ESC ] | 0x9d | OSC – Operating System Command
+			inputMode = _OSC;
+			break;
+		case 'X': //ESC X | 0x98 | SOS – Start of String
+			break;
+		case '^': //ESC ^ | 0x9e | PM – Privacy Message
+			break;
+		case '_': //ESC _ | 0x9f | APC – Application Program Command
+			break;
+		case 'c': //ESC c |      | RIS – Reset to Initial State
+			break;
+		default: break;
+	}
+}
+
+
+/// @brief DCS Mode
+/// @param byte 
+/// @return 
+void CmdMsgMgr::DCSMode(uint8_t byte)
+{
+
+}
+
+
+/// @brief CSI Mode
+/// @param byte 
+/// @return 
+void CmdMsgMgr::CSIMode(uint8_t byte)
+{
+	//Param byte
+	if (byte >= 0x30 && byte <= 0x3f)
+	{
+
+	}
+
+	//Middle byte
+	if (byte >= 0x20 && byte <= 0x2f)
+	{
+
+	}
+
+	//Final byte
+	if (byte >= 0x40 && byte <= 0x7e)
+	{
+		switch (byte)
+		{
+			case 'A': //up
+				if (NULL != cmdHistory[history-1])
+				{
+					RecordTempCmd();
+					history--;
+					RestoredHistory();
+				}
+				break;
+			case 'B': //down
+				if (history < cmd_history_size - 1)
+				{
+					history++;
+					RestoredHistory();
+				}
+				break;
+			case 'C': //right
+				if ('\0' != rxBuffer[rxBufPos])
+				{
+					txBuffer[txBufPos++] = rxBuffer[rxBufPos++];
+					inputMode = _INSERT;
+					return;
+				}
+				break;
+			case 'D': //left
+				if (rxBufPos)
+				{
+					rxBufPos--;
+					txBuffer[txBufPos++] = '\b';
+					inputMode = _INSERT;
+					return;
+				}
+				break;
+			case 'm': //SGR
+				break;
+			default: break;
+		}
+
+		inputMode = _EDIT;
+	}
+}
+
+
+/// @brief OSC Mode
+/// @param byte 
+/// @return 
+void CmdMsgMgr::OSCMode(uint8_t byte)
+{
+	if ('\0' == byte) inputMode = _EDIT;
 }
