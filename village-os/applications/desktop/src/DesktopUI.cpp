268f6127f7ba12f5c168e3b8d572cd2be0fe6ab3
aicodeone <277579066@qq.com>
2024-10-02 15:36:11 +0800

    desktop：整理代码

--- village-os/applications/desktop/src/DesktopUI.cpp
+++ village-os/applications/desktop/src/DesktopUI.cpp
@@ -26,65 +26,56 @@ DesktopUI::~DesktopUI()
 void DesktopUI::Setup()
 {
 	if (false == SetupWin()) return;
-
-	kernel->inputEvent.Attach(InputEvent::_OutputAxis, (Method)&DesktopUI::UpdateAxis, this);
 }
 
 
 /// @brief Exit
 void DesktopUI::Exit()
 {
-	kernel->inputEvent.Detach(InputEvent::_OutputAxis, (Method)&DesktopUI::UpdateAxis, this);
+
 }
 
 
 /// @brief SetupWin
 bool DesktopUI::SetupWin()
 {
-	VkGUI* vkgui = (VkGUI*)kernel->feature.GetModule("vkgui");
+	Module* module = kernel->feature.GetModule("vkgui");
+	if (NULL == module) return false;
 
-	if (NULL != vkgui)
-	{
-		mainwin = (Window*)vkgui->CreateMainWindow();
+	VkGUI* vkgui = (VkGUI*)module->GetData();
+	if (NULL == vkgui) return false;
+	
+	mainwin = (Window*)vkgui->CreateMainWindow();
 
-		tabbar = (Tabbar*)mainwin->CreateWedget(Wedget::_Tabbar);
-		tabbar->SetLocation(0, mainwin->GetHeight() - 40, mainwin->GetWidth(), 40);
+	tabbar = (Tabbar*)mainwin->CreateWedget(Wedget::_Tabbar);
+	tabbar->SetLocation(0, mainwin->GetHeight() - 40, mainwin->GetWidth(), 40);
 
-		button0 = (Button*)tabbar->CreateWedget(Wedget::_Button);
-		button0->SetLocation(0, 0, 40, 40);
-		button0->SetText((char*)"Start");
+	button0 = (Button*)tabbar->CreateWedget(Wedget::_Button);
+	button0->SetLocation(0, 0, 40, 40);
+	button0->SetText((char*)"Start");
 
-		button1 = (Button*)tabbar->CreateWedget(Wedget::_Button);
-		button1->SetLocation(50, 5, 30, 30);
-		button1->SetText((char*)"A");
+	button1 = (Button*)tabbar->CreateWedget(Wedget::_Button);
+	button1->SetLocation(50, 5, 30, 30);
+	button1->SetText((char*)"A");
 
-		button2 = (Button*)tabbar->CreateWedget(Wedget::_Button);
-		button2->SetLocation(90, 5, 30, 30);
-		button2->SetText((char*)"B");
+	button2 = (Button*)tabbar->CreateWedget(Wedget::_Button);
+	button2->SetLocation(90, 5, 30, 30);
+	button2->SetText((char*)"B");
 
-		button3 = (Button*)tabbar->CreateWedget(Wedget::_Button);
-		button3->SetLocation(130, 5, 30, 30);
-		button3->SetText((char*)"C");
+	button3 = (Button*)tabbar->CreateWedget(Wedget::_Button);
+	button3->SetLocation(130, 5, 30, 30);
+	button3->SetText((char*)"C");
 
-		button4 = (Button*)tabbar->CreateWedget(Wedget::_Button);
-		button4->SetLocation(170, 5, 30, 30);
-		button4->SetText((char*)"D");
+	button4 = (Button*)tabbar->CreateWedget(Wedget::_Button);
+	button4->SetLocation(170, 5, 30, 30);
+	button4->SetText((char*)"D");
 
-		cursor = (Cursor*)mainwin->CreateWedget(Wedget::_Cursor);
-		cursor->SetLocation(0, 0, mainwin->GetWidth(), mainwin->GetHeight());
+	cursor = (Cursor*)mainwin->CreateWedget(Wedget::_Cursor);
+	cursor->SetLocation(0, 0, mainwin->GetWidth(), mainwin->GetHeight());
 
-		mainwin->Show();
+	mainwin->Show();
 
-		return true;
-	}
-	return false;
-}
-
-
-/// @brief Update location axis
-void DesktopUI::UpdateAxis(InputEvent::OutputAxis* input)
-{
-	cursor->Update(input->axisX, input->axisY);
+	return true;
 }
 
 
@@ -94,5 +85,6 @@ void DesktopUI::Execute()
 	if (NULL != mainwin)
 	{
 		mainwin->Refresh();
+		kernel->thread.Sleep(50);
 	}
 }
