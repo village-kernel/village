94fb4059a93cdd0314adc7e8a6e41b1f4ad4e9d5
aicodeone <277579066@qq.com>
2025-01-08 21:26:49 +0800

    vg_textbox：增加Binding逻辑

--- village-gui/wedget/inc/vg_textbox.h
+++ village-gui/wedget/inc/vg_textbox.h
@@ -8,16 +8,33 @@
 #define __VG_TEXT_BOX_H__
 
 #include "vg_wedget.h"
-#include "vg_scrollbar.h"
+#include "vg_draw_text.h"
+#include "vg_input_data.h"
 
 
 /// @brief TextBox
 class TextBox : public Wedget
 {
+private:
+	//Memebers
+	char* 	      text;
+	IData<char*>* bText;
+
+	//Members
+	DrawText      drawText;
 public:
 	//Methods
 	TextBox();
 	~TextBox();
+
+	//Attribute Methods
+	void BindingText(IData<char*>* text);
+	void SetText(char* text);
+	char* GetText();
+
+	//Methods
+	void Initiate(VgDevices* devices);
+	void Redraw(DrawArea drawArea);
 };
 
 #endif //!__VG_TEXT_BOX_H__
--- village-gui/wedget/src/vg_textbox.cpp
+++ village-gui/wedget/src/vg_textbox.cpp
@@ -9,6 +9,8 @@
 
 /// @brief Constructor
 TextBox::TextBox()
+	:text(NULL),
+	bText(NULL)
 {
 	SetTitle((char*)"textbox");
 }
@@ -17,4 +19,49 @@ TextBox::TextBox()
 /// @brief Destructor
 TextBox::~TextBox()
 {
+}
+
+
+/// @brief TextBox binding text
+/// @param text 
+void TextBox::BindingText(IData<char*>* text)
+{
+	bText = text; if (bText) bText->Binding(this);
+}
+
+
+/// @brief TextBox set text
+/// @param text 
+void TextBox::SetText(char* text)
+{
+	this->text = text; if (bText) bText->Set(text);
+}
+
+
+/// @brief TextBox get text
+/// @return 
+char* TextBox::GetText()
+{
+	return bText ? bText->Get() : text;
+}
+
+
+/// @brief TextBox initiate
+/// @param devices 
+void TextBox::Initiate(VgDevices* devices)
+{
+	Wedget::Initiate(devices);
+
+	drawText.Initiate(devices);
+	drawText.SetAlignment(DrawText::_AlignLeft);
+}
+
+
+/// @brief TextBox redraw
+/// @param drawArea 
+void TextBox::Redraw(DrawArea drawArea)
+{
+	Wedget::Redraw(drawArea);
+
+	drawText.Execute(layerArea, drawArea, GetText());
 }
