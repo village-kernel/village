75002027db6ee49c5da03ad7411bb036c1f7de4c
aicodeone <277579066@qq.com>
2025-01-18 18:03:22 +0800

    village：去除与图形库相关的代码，简化工程

---
+++ village-demo/programs/demo/inc/demo_app.h
@@ -0,0 +1,12 @@
+//###########################################################################
+// demo_app.h
+// Declarations of the demo app
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#ifndef __DEMO_APP_H__
+#define __DEMO_APP_H__
+
+#include "vk_kernel.h"
+
+#endif //!__DEMO_APP_H__
