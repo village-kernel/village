8100bfb1888f8d9ac09132eb2a6d1d0b4fbd9514
aicodeone <277579066@qq.com>
2025-02-05 16:02:04 +0800

    vk_drv_dma：使用ll库重新编写

--- village-kernel/drivers/platform/stm32h7xx/src/vk_drv_dma.cpp
+++ village-kernel/drivers/platform/stm32h7xx/src/vk_drv_dma.cpp
@@ -9,85 +9,35 @@
 
 /// @brief Constructor
 Dma::Dma()
-    :muxStatusReg(NULL),
-    muxChannelReg(NULL),
-    streamReg(NULL),
-    commonReg(NULL),
-    flagClearReg(NULL),
-    statusReg(NULL),
-    flagOffset(0)
+    : DMAx(NULL),
+      stream(0)
 {
 }
 
 
 /// @brief Selects the DMA channel number
 /// @param group 
-/// @param channel 
+/// @param stream 
 void Dma::Initialize(uint8_t group, uint8_t stream)
 {
     if (_Group1 == group)
     {
-        //Enable dma1 clock
-        RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
-
-        //Gets the DMA and stream register
-        commonReg = DMA1;
-        streamReg = DMA1_Stream0 + stream;
-
-        //Gets the DMAMUX and DMAMUX status register
-        muxStatusReg = DMAMUX1_ChannelStatus;
-        muxChannelReg = DMAMUX1_Channel0 + stream;
+        // Enable DMA1 clock
+        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
+        DMAx = DMA1;
     }
     else if (_Group2 == group)
     {
-        //Enable dma2 clock
-        RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-        
-        //Gets the DMA and stream register
-        commonReg = DMA2;
-        streamReg = DMA2_Stream0 + stream;
-        
-        //Gets the DMAMUX and DMAMUX status register
-        muxStatusReg = DMAMUX1_ChannelStatus;
-        muxChannelReg = DMAMUX1_Channel0 + stream + 8;
+        // Enable DMA2 clock
+        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA2);
+        DMAx = DMA2;
     }
 
-    if (stream <= _Stream3)
-    {
-        //Gets the status and flag clear register
-        statusReg = &(commonReg->LISR);
-        flagClearReg = &(commonReg->LIFCR);
-        
-        //Calaculate the flag offset
-        if (stream == _Stream0 || stream == _Stream1)
-            flagOffset = stream * 6;
-        else
-            flagOffset = stream * 6 + 4;
-    }
-    else if (stream >= _Stream4)
-    {
-        //Gets the status and flag clear register
-        statusReg = &(commonReg->HISR);
-        flagClearReg = &(commonReg->HIFCR);
-        
-        //Calaculate the flag offset
-        if (stream == _Stream4 || stream == _Stream5)
-            flagOffset = (stream - _Stream4) * 6;
-        else
-            flagOffset = (stream - _Stream4) * 6 + 4;
-    }
+    // Set stream index
+    this->stream = stream;
 
-    //Disable dma
-    streamReg->CR &= ~DMA_SxCR_EN;
-}
-
-
-/// @brief Sets the DMA burst transfer
-/// @param transfer 
-void Dma::ConfigBurstTransfer(DmaMemBurstTrans transfer)
-{
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_MBURST_Msk) | (transfer << DMA_SxCR_MBURST_Pos);
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_PBURST_Msk) | (transfer << DMA_SxCR_PBURST_Pos);
+    // Disable DMA stream
+    LL_DMA_DisableStream(DMAx, stream);
 }
 
 
@@ -95,7 +45,7 @@ void Dma::ConfigBurstTransfer(DmaMemBurstTrans transfer)
 /// @param dmaChPriority 
 void Dma::ConfigPriority(DmaChPriority dmaChPriority)
 {
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_PL_Msk) | (dmaChPriority << DMA_SxCR_PL_Pos);
+    LL_DMA_SetStreamPriorityLevel(DMAx, stream, dmaChPriority);
 }
 
 
@@ -104,10 +54,9 @@ void Dma::ConfigPriority(DmaChPriority dmaChPriority)
 /// @param dmaDataSize 
 void Dma::ConfigDirAndDataWidth(DmaDatDir dmaDataDir, DmaDataSize dmaDataSize)
 {
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_DIR_Msk) | (dmaDataDir << DMA_SxCR_DIR_Pos);
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_MSIZE_Msk) | (dmaDataSize << DMA_SxCR_MSIZE_Pos);
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_PSIZE_Msk) | (dmaDataSize << DMA_SxCR_PSIZE_Pos);
-    streamReg->FCR = (streamReg->FCR & ~DMA_SxFCR_FTH_Msk);
+    LL_DMA_SetDataTransferDirection(DMAx, stream, dmaDataDir);
+    LL_DMA_SetMemorySize(DMAx, stream, dmaDataSize);
+    LL_DMA_SetPeriphSize(DMAx, stream, dmaDataSize);
 }
 
 
@@ -116,8 +65,8 @@ void Dma::ConfigDirAndDataWidth(DmaDatDir dmaDataDir, DmaDataSize dmaDataSize)
 /// @param enaPeriphInc 
 void Dma::ConfigIncMode(bool enaMemInc, bool enaPeriphInc)
 {
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_MINC_Msk) | (enaMemInc << DMA_SxCR_MINC_Pos);
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_PINC_Msk) | (enaPeriphInc << DMA_SxCR_PINC_Pos);
+    LL_DMA_SetMemoryIncMode(DMAx, stream, enaMemInc ? LL_DMA_MEMORY_INCREMENT : LL_DMA_MEMORY_NOINCREMENT);
+    LL_DMA_SetPeriphIncMode(DMAx, stream, enaPeriphInc ? LL_DMA_PERIPH_INCREMENT : LL_DMA_PERIPH_NOINCREMENT);
 }
 
 
@@ -125,55 +74,67 @@ void Dma::ConfigIncMode(bool enaMemInc, bool enaPeriphInc)
 /// @param isEnableCircularMode 
 void Dma::ConfigCircularMode(bool isEnableCircularMode)
 {
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_CIRC_Msk) | (isEnableCircularMode << DMA_SxCR_CIRC_Pos);
+    LL_DMA_SetMode(DMAx, stream, isEnableCircularMode ? LL_DMA_MODE_CIRCULAR : LL_DMA_MODE_NORMAL);
 }
 
 
-/// @brief Enable or disable DMA related interrupts, including interrupt when:
-///        transfer error occurs, transfer half complete, transfer fully complete
-/// @param enaXferErr 
-/// @param enaHalfXfer 
-/// @param enaFullXfer 
-void Dma::ConfigInterrupts(bool enaXferErr, bool enaHalfXfer, bool enaFullXfer)
-{
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_TEIE_Msk) | (enaXferErr << DMA_SxCR_TEIE_Pos);
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_HTIE_Msk) | (enaHalfXfer << DMA_SxCR_HTIE_Pos);
-    streamReg->CR = (streamReg->CR & ~DMA_SxCR_TCIE_Msk) | (enaFullXfer << DMA_SxCR_TCIE_Pos);
-}
-
-
-/// @brief Configure DMA request, must be configured, otherwise you can not use DMA transfer
+/// @brief Configure DMA request
 /// @param request 
 void Dma::ConfigRequest(uint8_t request)
 {
-    muxChannelReg->CCR = (muxChannelReg->CCR & ~DMAMUX_CxCR_DMAREQ_ID_Msk) | (request << DMAMUX_CxCR_DMAREQ_ID_Pos);
+    LL_DMA_SetPeriphRequest(DMAx, stream, request << DMAMUX_CxCR_DMAREQ_ID_Pos);
 }
 
 
-/// @brief Checks if a transfer is ready, this function should not be used in circular mode
+/// @brief Get transfer complete flag
+/// @return 
+bool Dma::GetTransferCompleteFlag()
+{
+    if (_Stream0 == stream)
+        return LL_DMA_IsActiveFlag_TC0(DMAx);
+    else if (_Stream1 == stream)
+        return LL_DMA_IsActiveFlag_TC1(DMAx);
+    else if (_Stream2 == stream)
+        return LL_DMA_IsActiveFlag_TC2(DMAx);
+    else if (_Stream3 == stream)
+        return LL_DMA_IsActiveFlag_TC3(DMAx);
+    else if (_Stream4 == stream)
+        return LL_DMA_IsActiveFlag_TC4(DMAx);
+    else if (_Stream5 == stream)
+        return LL_DMA_IsActiveFlag_TC5(DMAx);
+    else if (_Stream6 == stream)
+        return LL_DMA_IsActiveFlag_TC6(DMAx);
+    else if (_Stream7 == stream)
+        return LL_DMA_IsActiveFlag_TC7(DMAx);
+    return false;
+}
+
+
+/// @brief Clear transfer complete flag
+void Dma::ClearTransferCompleteFlag()
+{
+    if (_Stream0 == stream)
+        LL_DMA_ClearFlag_TC0(DMAx);
+    else if (_Stream1 == stream)
+        LL_DMA_ClearFlag_TC1(DMAx);
+    else if (_Stream2 == stream)
+        LL_DMA_ClearFlag_TC2(DMAx);
+    else if (_Stream3 == stream)
+        LL_DMA_ClearFlag_TC3(DMAx);
+    else if (_Stream4 == stream)
+        LL_DMA_ClearFlag_TC4(DMAx);
+    else if (_Stream5 == stream)
+        LL_DMA_ClearFlag_TC5(DMAx);
+    else if (_Stream6 == stream)
+        LL_DMA_ClearFlag_TC6(DMAx);
+    else if (_Stream7 == stream)
+        LL_DMA_ClearFlag_TC7(DMAx);
+}
+
+
+/// @brief Checks if a transfer is ready
 /// @return 
 bool Dma::IsReady()
 {
-    if (IsEnable() && !GetTransferCompleteFlag())
-    {
-        return false;
-    }
-    return true;
-}
-
-
-/// @brief Attempts to start transfer, if DMA is not currently busy
-///        Returns whether transfer was initiated successfully
-///        This method should not be used in circular mode, simply call Enable()
-/// @return 
-bool Dma::StartTransfer()
-{
-    if (IsReady())
-    {
-        Disable();
-        ClearTransferCompleteFlag();
-        Enable();
-        return true;
-    }
-    return false;
+    return !(IsEnable() && !GetTransferCompleteFlag());
 }
