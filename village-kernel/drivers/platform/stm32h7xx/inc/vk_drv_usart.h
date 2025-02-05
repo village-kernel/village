8c23dced8d05721c31d77616cc20dcd87c557c09
aicodeone <277579066@qq.com>
2025-02-05 16:38:23 +0800

    vk_drv_usart：使用ll库重新编写

--- village-kernel/drivers/platform/stm32h7xx/inc/vk_drv_usart.h
+++ village-kernel/drivers/platform/stm32h7xx/inc/vk_drv_usart.h
@@ -7,120 +7,87 @@
 #ifndef __VK_DRV_USART_H__
 #define __VK_DRV_USART_H__
 
-#include "stm32h7xx.h"
-#include "vk_drv_gpio.h"
-
+#include "stm32h7xx_ll_usart.h"
+#include "stm32h7xx_ll_bus.h"
 
 /// @brief Usart
 class Usart
 {
 public:
-	//Enumerations
-	enum Channel
-	{
-		_Usart1 = 1,
-		_Usart2 = 2,
-		_Usart3 = 3,
-		_Usart4 = 4,
-		_Usart5 = 5,
-	};
+    //Enumerations
+    enum Channel
+    {
+        _Usart1 = 1,
+        _Usart2 = 2,
+        _Usart3 = 3,
+        _Usart4 = 4,
+        _Usart5 = 5,
+    };
 
-	enum DataBits
-	{
-		_8Bits = 0,
-		_9Bits = 1,
-	};
+    enum DataBits
+    {
+        _8Bits = LL_USART_DATAWIDTH_8B,
+        _9Bits = LL_USART_DATAWIDTH_9B,
+    };
 
-	enum Parity
-	{
-		_NoParity = 0,
-		_Parity = 1,
-	};
+    enum Parity
+    {
+        _NoParity = LL_USART_PARITY_NONE,
+        _Parity = LL_USART_PARITY_EVEN,
+    };
 
-	enum StopBits
-	{
-		_1Stop = 0b00,
-		_HalfStop = 0b01,
-		_2Stop = 0b10,
-		_1AndHalfStop = 0b11,
-	};
-
-	enum InterruptType
-	{
-		_IdleInterrupt = 4,
-		_RecDateRegNotEmpty,
-		_TransComplete,
-		_TransDataRegEmpty,
-	};
+    enum StopBits
+    {
+        _1Stop = LL_USART_STOPBITS_1,
+        _HalfStop = LL_USART_STOPBITS_0_5,
+        _2Stop = LL_USART_STOPBITS_2,
+        _1AndHalfStop = LL_USART_STOPBITS_1_5,
+    };
 private:
-	//Static Constants
-	static const uint8_t interruptSize = 5;
-
-	//Members
-	volatile USART_TypeDef* base;
+    //Members
+    USART_TypeDef* UARTx;
 public:
-	//Methods
-	Usart();
-	void Initialize(Channel channel);
-	void ConfigPortSettings(DataBits dataBits, Parity parity, StopBits stopBits);
-	void ConfigDriverEnableMode(bool usingDEM = true, bool polarity = false);
-	void ConfigReceiverTimeout(bool enable, uint32_t rto = 0, uint8_t blen = 0);
-	void ConfigDma(bool dmaTxEnable = true, bool dmaRxEnable = true);
-	void SetBaudRate(uint32_t baudRate, bool over8 = false);
-	void CheckError();
-	int Write(uint8_t* data, uint32_t size, uint32_t offset);
-	int Read(uint8_t* data, uint32_t size, uint32_t offset);
+    //Methods
+    Usart();
+    void Initialize(Channel channel);
+    void ConfigPortSettings(DataBits dataBits, Parity parity, StopBits stopBits);
+    void ConfigDriverEnableMode(bool usingDEM = true, bool polarity = false);
+    void ConfigReceiverTimeout(bool enable, uint32_t rto = 0, uint8_t blen = 0);
+    void ConfigDma(bool dmaTxEnable = true, bool dmaRxEnable = true);
+    void SetBaudRate(uint32_t baudRate, bool over8 = false);
+    void CheckError();
+    int Write(uint8_t* data, uint32_t size, uint32_t offset);
+    int Read(uint8_t* data, uint32_t size, uint32_t offset);
 
-	///Enables operation of the USART peripheral
-	inline void Enable() { base->CR1 |= USART_CR1_UE; }
+    ///Enables operation of the USART peripheral
+    inline void Enable() { LL_USART_Enable(UARTx); }
 
-	///Disables operation of the USART peripheral
-	inline void Disable() { base->CR1 &= ~USART_CR1_UE; }
+    ///Disables operation of the USART peripheral
+    inline void Disable() { LL_USART_Disable(UARTx); }
 
-	///Configure interrupt
-	inline void ConfigInterrupt(InterruptType type) { base->CR1 |= (1 << type); }
+    ///Gets the address of the transmit data register
+    inline uint32_t* GetTxAddr() { return (uint32_t*)&(UARTx->TDR); }
 
-	///Gets the address of the transmit data register
-	inline uint32_t* GetTxAddr() { return (uint32_t*)&(base->TDR); }
+    ///Gets the address of the receive data register
+    inline uint32_t* GetRxAddr() { return (uint32_t*)&(UARTx->RDR); }
 
-	///Gets the address of the receive data register
-	inline uint32_t* GetRxAddr() { return (uint32_t*)&(base->RDR); }
+    ///Checks for Framing Errors
+    inline bool CheckFramingError() { return LL_USART_IsActiveFlag_FE(UARTx); }
 
-	///Checks for Framing Errors
-	inline bool CheckFramingError() { return (base->ISR & USART_ISR_FE_Msk); }
+    ///Clears the Framing Error flag
+    inline void ClearFramingError() { LL_USART_ClearFlag_FE(UARTx); }
 
-	///Clears the Framing Error flag
-	inline void ClearFramingError() { base->ICR |= USART_ICR_FECF_Msk; }
+    ///Clears the receiver timeout flag
+    inline void ClearReceiverTimeoutFlag() { LL_USART_ClearFlag_RTO(UARTx); }
 
-	///Clears the trans complete flag
-	inline void ClearTransCompleteFlag() { base->ICR |= USART_ICR_TCCF_Msk; }
+    ///Check if the send register is empty
+    inline bool IsTxRegisterEmpty() { return LL_USART_IsActiveFlag_TXE(UARTx); }
 
-	///Clears the trans data register empty flag
-	inline void ClearTransDataRegEmptyFlag() { base->RQR |= USART_RQR_TXFRQ_Msk; }
+    ///Check if the read date register not empty
+    inline bool IsReadDataRegNotEmpty() { return LL_USART_IsActiveFlag_RXNE(UARTx); }
 
-	///Clears the read data register not empty flag
-	inline void ClearReadDataRegNotEmptyFlag() { base->RQR |= USART_RQR_RXFRQ_Msk; }
-
-	///Clears the idle line detected flag
-	inline void ClearIdleLineDetectedFlag() { base->ICR |= USART_ICR_IDLECF_Msk; }
-
-	///Clears the receiver timeout flag
-	inline void ClearReceiverTimeoutFlag() { base->ICR |= USART_ICR_RTOCF_Msk; }
-
-	///Check if the send register is empty
-	inline bool IsTxRegisterEmpty() { return bool(base->ISR & USART_ISR_TXE_TXFNF_Msk); }
-
-	///Check if the tarnsmission complete
-	inline bool IsTransComplete() { return bool(base->ISR & USART_ISR_TC_Msk); }
-
-	///Check if the read date register not empty
-	inline bool IsReadDataRegNotEmpty() { return bool(base->ISR & USART_ISR_RXNE_RXFNE_Msk); }
-
-	///Check if usart idle status
-	inline bool IsUsartIdleStauts() { return bool(base->ISR & USART_ISR_IDLE_Msk); }
-
-	///Check if receiver timeout
-	inline bool IsReceiverTimeout() { return bool(base->ISR & USART_ISR_RTOF_Msk); }
+    ///Check if receiver timeout
+    inline bool IsReceiverTimeout() { return LL_USART_IsActiveFlag_RTO(UARTx); }
 };
 
 #endif // !__VK_DRV_USART_H__
--- village-kernel/drivers/platform/stm32h7xx/src/vk_drv_usart.cpp
+++ village-kernel/drivers/platform/stm32h7xx/src/vk_drv_usart.cpp
@@ -6,10 +6,9 @@
 //###########################################################################
 #include "vk_drv_usart.h"
 
-
 /// @brief Constructor
 Usart::Usart()
-	: base(NULL)
+    : UARTx(NULL)
 {
 }
 
@@ -18,53 +17,39 @@ Usart::Usart()
 /// @param channel 
 void Usart::Initialize(Channel channel)
 {
-	//Enable the peripheral clock and assign the base pointer
-	if (_Usart1 == channel)
-	{
-		RCC->APB2RSTR |= RCC_APB2RSTR_USART1RST;
-		RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;
-		
-		RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
-		base = USART1;
-	}
-	else if (_Usart2 == channel)
-	{
-		RCC->APB1LRSTR |= RCC_APB1LRSTR_USART2RST;
-		RCC->APB1LRSTR &= ~RCC_APB1LRSTR_USART2RST;
+    //Enable the peripheral clock and assign the UARTx pointer
+    if (_Usart1 == channel)
+    {
+        LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1);
+        UARTx = USART1;
+    }
+    else if (_Usart2 == channel)
+    {
+        LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART2);
+        UARTx = USART2;
+    }
+    else if (_Usart3 == channel)
+    {
+        LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART3);
+        UARTx = USART3;
+    }
+    else if (_Usart4 == channel)
+    {
+        LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART4);
+        UARTx = UART4;
+    }
+    else if (_Usart5 == channel)
+    {
+        LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART5);
+        UARTx = UART5;
+    }
 
-		RCC->APB1LENR |= RCC_APB1LENR_USART2EN;
-		base = USART2; 
-	}
-	else if (_Usart3 == channel)
-	{
-		RCC->APB1LRSTR |= RCC_APB1LRSTR_USART3RST;
-		RCC->APB1LRSTR &= ~RCC_APB1LRSTR_USART3RST;
+    //Enable FIFO mode
+    LL_USART_EnableFIFO(UARTx);
 
-		RCC->APB1LENR |= RCC_APB1LENR_USART3EN;
-		base = USART3;
-	}
-	else if (_Usart4 == channel)
-	{
-		RCC->APB1LRSTR |= RCC_APB1LRSTR_UART4RST;
-		RCC->APB1LRSTR &= ~RCC_APB1LRSTR_UART4RST;
-
-		RCC->APB1LENR |= RCC_APB1LENR_UART4EN;
-		base = UART4;
-	}
-	else if (_Usart5 == channel)
-	{
-		RCC->APB1LRSTR |= RCC_APB1LRSTR_UART5RST;
-		RCC->APB1LRSTR &= ~RCC_APB1LRSTR_UART5RST;
-
-		RCC->APB1LENR |= RCC_APB1LENR_UART5EN;
-		base = UART5;
-	}
-
-	//Enable FIFO mode
-	base->CR1 |= USART_CR1_FIFOEN_Msk;
-
-	//Enable Receiver and Transmitter
-	base->CR1 |= USART_CR1_RE_Msk | USART_CR1_TE_Msk;
+    //Enable Receiver and Transmitter
+    LL_USART_SetTransferDirection(UARTx, LL_USART_DIRECTION_TX_RX);
+    LL_USART_Enable(UARTx);
 }
 
 
@@ -74,30 +59,18 @@ void Usart::Initialize(Channel channel)
 /// @param stopBits 
 void Usart::ConfigPortSettings(DataBits dataBits, Parity parity, StopBits stopBits)
 {
-	//Configure word size
-	if (_9Bits == dataBits)
-	{
-		base->CR1 |= USART_CR1_M0;
-	}
-	else if (_8Bits == dataBits)
-	{
-		base->CR1 &= ~USART_CR1_M0;
-	}
+    //Configure word size
+    LL_USART_SetDataWidth(UARTx, dataBits);
 
-	//Configure parity
-	if (_Parity == parity)
-		base->CR1 |= USART_CR1_PCE;
-	else
-		base->CR1 &= ~USART_CR1_PCE;
+    //Configure parity
+    LL_USART_SetParity(UARTx, parity);
 
-	//Configure stop bits
-	base->CR2 = (base->CR2 & ~USART_CR2_STOP_Msk) | (stopBits << USART_CR2_STOP_Pos);
+    //Configure stop bits
+    LL_USART_SetStopBitsLength(UARTx, stopBits);
 }
 
 
 /// @brief Sets the baud rate of the serial bus
-///        baudRate indicates the desired baudRate
-///        over8 indicates the whether oversampling by 8 will be used (otherwise oversampling of 16)
 /// @param baudRate 
 /// @param over8 
 void Usart::SetBaudRate(uint32_t baudRate, bool over8)
@@ -111,23 +84,23 @@ void Usart::SetBaudRate(uint32_t baudRate, bool over8)
 		//Calculate the baudrate of 8x Oversampling
 		if (over8)
 		{
-			base->CR1 |= USART_CR1_OVER8;
-			base->BRR = (PCLK1Freq * 2 + baudRate / 2) / baudRate;
+			UARTx->CR1 |= USART_CR1_OVER8;
+			UARTx->BRR = (PCLK1Freq * 2 + baudRate / 2) / baudRate;
 		}
 		//Calculate the baudrate of 16x Oversampling 
 		else
 		{
-			base->CR1 &= ~USART_CR1_OVER8;
-			base->BRR = (PCLK1Freq + baudRate / 2) / baudRate;
+			UARTx->CR1 &= ~USART_CR1_OVER8;
+			UARTx->BRR = (PCLK1Freq + baudRate / 2) / baudRate;
 		}
 	}
 	else
 	{
 		//Measurement of the start bit is used to detect the baud rate
-		base->CR2 = (base->CR2 & ~USART_CR2_ABRMODE_Msk);
+		UARTx->CR2 = (UARTx->CR2 & ~USART_CR2_ABRMODE_Msk);
 
 		//Auto baud rate enable
-		base->CR2 |= USART_CR2_ABREN;
+		UARTx->CR2 |= USART_CR2_ABREN;
 	}
 }
 
@@ -137,8 +110,12 @@ void Usart::SetBaudRate(uint32_t baudRate, bool over8)
 /// @param polarity 
 void Usart::ConfigDriverEnableMode(bool usingDEM, bool polarity)
 {
-	base->CR3 = (base->CR3 & ~USART_CR3_DEM_Msk) | ((usingDEM ? 1 : 0) << USART_CR3_DEM_Pos);
-	base->CR3 = (base->CR3 & ~USART_CR3_DEP_Msk) | ((polarity ? 1 : 0) << USART_CR3_DEP_Pos);
+    if (usingDEM)
+        LL_USART_EnableDEMode(UARTx);
+    else
+        LL_USART_DisableDEMode(UARTx);
+
+    LL_USART_SetDESignalPolarity(UARTx, polarity ? LL_USART_DE_POLARITY_HIGH : LL_USART_DE_POLARITY_LOW);
 }
 
 
@@ -148,9 +125,13 @@ void Usart::ConfigDriverEnableMode(bool usingDEM, bool polarity)
 /// @param blen 
 void Usart::ConfigReceiverTimeout(bool enable, uint32_t rto, uint8_t blen)
 {
-	base->CR2 = (base->CR2 & ~USART_CR2_RTOEN_Msk) | ((enable ? 1 : 0) << USART_CR2_RTOEN_Pos);
-	base->RTOR = (base->RTOR & ~USART_RTOR_BLEN_Msk) | (blen << USART_RTOR_BLEN_Pos);
-	base->RTOR = (base->RTOR & ~USART_RTOR_RTO_Msk) | (rto << USART_RTOR_RTO_Pos);
+    if (enable)
+        LL_USART_EnableRxTimeout(UARTx);
+    else
+        LL_USART_DisableRxTimeout(UARTx);
+        
+    LL_USART_SetRxTimeout(UARTx, rto);
+    LL_USART_SetBlockLength(UARTx, blen);
 }
 
 
@@ -159,11 +140,15 @@ void Usart::ConfigReceiverTimeout(bool enable, uint32_t rto, uint8_t blen)
 /// @param dmaRxEnable 
 void Usart::ConfigDma(bool dmaTxEnable, bool dmaRxEnable)
 {
-	if (dmaTxEnable) base->CR3 |= USART_CR3_DMAT;
-	else base->CR3 &= ~USART_CR3_DMAT;
+    if (dmaTxEnable)
+        LL_USART_EnableDMAReq_TX(UARTx);
+    else
+        LL_USART_DisableDMAReq_TX(UARTx);
 
-	if (dmaRxEnable) base->CR3 |= USART_CR3_DMAR;
-	else base->CR3 &= ~USART_CR3_DMAR;
+    if (dmaRxEnable)
+        LL_USART_EnableDMAReq_RX(UARTx);
+    else
+        LL_USART_DisableDMAReq_RX(UARTx);
 }
 
 
@@ -174,20 +159,20 @@ void Usart::ConfigDma(bool dmaTxEnable, bool dmaRxEnable)
 /// @return 
 int Usart::Write(uint8_t* data, uint32_t size, uint32_t offset)
 {
-	uint16_t loop = size;
+    uint16_t loop = size;
 
-	data += offset;
+    data += offset;
 
-	while (loop)
-	{
-		if (IsTxRegisterEmpty())
-		{
-			base->TDR = *data++;
-			loop--;
-		}
-	}
+    while (loop)
+    {
+        if (IsTxRegisterEmpty())
+        {
+            LL_USART_TransmitData8(UARTx, *data++);
+            loop--;
+        }
+    }
 
-	return size;
+    return size;
 }
 
 
@@ -198,25 +183,29 @@ int Usart::Write(uint8_t* data, uint32_t size, uint32_t offset)
 /// @return 
 int Usart::Read(uint8_t* data, uint32_t size, uint32_t offset)
 {
-	uint32_t length = 0;
+    uint32_t length = 0;
 
-	data += offset;
+    data += offset;
 
-	while (IsReadDataRegNotEmpty())
-	{
-		*data++ = base->RDR;
-		if (++length >= size) break;
-	}
+    while (IsReadDataRegNotEmpty())
+    {
+        *data++ = LL_USART_ReceiveData8(UARTx);
+        if (++length >= size) break;
+    }
 
-	return length;
+    return length;
 }
 
 
 /// @brief Check error
 void Usart::CheckError()
 {
-	if (base->ISR & (USART_ISR_PE | USART_ISR_FE | USART_ISR_NE | USART_ISR_ORE))
-	{
-		base->ICR = (USART_ICR_PECF | USART_ICR_FECF | USART_ICR_NECF | USART_ICR_ORECF);
-	}
+    if (LL_USART_IsActiveFlag_PE(UARTx) || LL_USART_IsActiveFlag_FE(UARTx) ||
+        LL_USART_IsActiveFlag_NE(UARTx) || LL_USART_IsActiveFlag_ORE(UARTx))
+    {
+        LL_USART_ClearFlag_PE(UARTx);
+        LL_USART_ClearFlag_FE(UARTx);
+        LL_USART_ClearFlag_NE(UARTx);
+        LL_USART_ClearFlag_ORE(UARTx);
+    }
 }
